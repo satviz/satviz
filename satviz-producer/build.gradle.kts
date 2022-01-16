860d59667d0957b24fc01824860f9a00897e8657
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-16 18:28:58 +0100

    Fix ipasir4j dependency declaration

--- satviz-producer/build.gradle.kts
+++ satviz-producer/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 repositories {
-    maven("https://repo.devcord.club")
+    maven("https://repo.devcord.club/snapshots")
 }
 
 dependencies {
-    implementation("edu.kit.satviz:ipasir4j:0.1.0")
+    implementation("edu.kit.satviz:ipasir4j:0.1.0-SNAPSHOT")
     implementation(project(":satviz-common"))
     implementation(project(":satviz-network"))
     implementation(project(":satviz-parsers"))
