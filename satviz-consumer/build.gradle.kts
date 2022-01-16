d75ad8abd5da9a1eb3d28c690faa35131c753d1c
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-16 18:11:58 +0100

    Collapse consumer-modules

--- satviz-consumer/build.gradle.kts
+++ satviz-consumer/build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
     java
+    id("org.openjfx.javafxplugin") version "0.0.10"
     id("org.beryx.jlink") version "2.24.4"
 }
 
 dependencies {
-    implementation(project(":satviz-config"))
-    implementation(project(":satviz-gui"))
-    implementation(project(":satviz-processing"))
+    implementation(project(":satviz-network"))
+    implementation(project(":satviz-sat"))
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("net.sourceforge.argparse4j:argparse4j:0.9.0")
     implementation(project(":satviz-parsers"))
+}
+
+javafx {
+    modules("javafx.controls", "javafx.fxml")
 }
