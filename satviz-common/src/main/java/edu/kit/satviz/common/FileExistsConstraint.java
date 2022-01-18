f3de14825d4a72f2e6a764d058bce1481aea4d45
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-18 10:37:17 +0100

    Implement FileExistsConstraint

--- satviz-common/src/main/java/edu/kit/satviz/common/FileExistsConstraint.java
+++ satviz-common/src/main/java/edu/kit/satviz/common/FileExistsConstraint.java
@@ -1,10 +1,20 @@
 package edu.kit.satviz.common;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileExistsConstraint implements Constraint<Path> {
+
+  private final boolean mustExist;
+
+  public FileExistsConstraint(boolean mustExist) {
+    this.mustExist = mustExist;
+  }
+
   @Override
   public void validate(Path obj) throws ConstraintValidationException {
-
+    if (Files.exists(obj) ^ mustExist) {
+      fail(obj + (mustExist ? " does not exist" : " already exists"));
+    }
   }
 }
