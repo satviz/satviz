266f568a346a48ebda181449baf17e08b057b8f1
LuWae <l.waeldele@t-online.de>
2022-01-18 15:15:45 +0100

    serializer checked exceptions

--- satviz-serial/src/main/java/edu/kit/satviz/serial/Serializer.java
+++ satviz-serial/src/main/java/edu/kit/satviz/serial/Serializer.java
@@ -1,13 +1,14 @@
 package edu.kit.satviz.serial;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public abstract class Serializer<T> {
 
-  public abstract void serialize(T t, OutputStream out);
+  public abstract void serialize(T t, OutputStream out) throws IOException, SerializationException;
 
-  public abstract T deserialize(InputStream in);
+  public abstract T deserialize(InputStream in) throws IOException, SerializationException;
 
   public abstract SerialBuilder<T> getBuilder();
 }
