4e2afbb7e265dd50a8b41ca6ec71f1999269e2f0
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-18 10:48:40 +0100

    Remove code smell and add checking method to Constraint

--- satviz-common/src/main/java/edu/kit/satviz/common/Constraint.java
+++ satviz-common/src/main/java/edu/kit/satviz/common/Constraint.java
@@ -1,13 +1,22 @@
 package edu.kit.satviz.common;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public interface Constraint<T> {
 
   void validate(T obj) throws ConstraintValidationException;
 
   default <U> Constraint<U> on(Function<U, T> mapper) {
-    return (obj) -> validate(mapper.apply(obj));
+    return obj -> validate(mapper.apply(obj));
+  }
+
+  static <T> Constraint<T> checking(Predicate<T> predicate, String message) {
+    return obj -> {
+      if (!predicate.test(obj)) {
+        throw new ConstraintValidationException(message);
+      }
+    };
   }
 
   default void fail(String message) throws ConstraintValidationException {
@@ -16,7 +25,7 @@ public interface Constraint<T> {
 
   @SafeVarargs
   static <T> Constraint<T> allOf(Constraint<T>... constraints) {
-    return (obj) -> {
+    return obj -> {
       for (Constraint<T> constraint : constraints) {
         constraint.validate(obj);
       }
