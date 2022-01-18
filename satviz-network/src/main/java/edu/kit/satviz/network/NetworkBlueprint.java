9aecd0f01bbd80bdc4d7c930314fbb2972092d25
LuWae <l.waeldele@t-online.de>
2022-01-18 14:25:21 +0100

    added NetworkBlueprint and Receiver

--- satviz-network/src/main/java/edu/kit/satviz/network/NetworkBlueprint.java
+++ satviz-network/src/main/java/edu/kit/satviz/network/NetworkBlueprint.java
@@ -1,4 +1,54 @@
 package edu.kit.satviz.network;
 
+import edu.kit.satviz.serial.SerialBuilder;
+import edu.kit.satviz.serial.Serializer;
+import java.io.OutputStream;
+import java.util.Map;
+
+/**
+ * A mapping of message types to transmitted objects.
+ *
+ * @author luwae
+ */
 public class NetworkBlueprint {
+  private final Map<Byte, Serializer<?>> typeMap;
+
+  /**
+   * Creates a new blueprint with specified type mapping.
+   *
+   * @param typeMap the mapping from type to serializer
+   */
+  public NetworkBlueprint(Map<Byte, Serializer<?>> typeMap) {
+    this.typeMap = typeMap;
+  }
+
+  /**
+   * Serializes an object according to its type.
+   *
+   * @param obj the object
+   * @param type the object's type
+   * @param out the stream to serialize to
+   */
+  public void serialize(Object obj, byte type, OutputStream out) {
+    @SuppressWarnings("unchecked")
+    Serializer<Object> s = (Serializer<Object>) typeMap.get(type);
+    if (s != null) {
+      s.serialize(obj, out);
+    }
+  }
+
+  /**
+   * Returns a deserialization builder according to the given type.
+   * TODO should not return null, always a valid builder.
+   *
+   * @param type the type
+   * @return a new builder for the given type of objects
+   */
+  public SerialBuilder<?> getBuilder(int type) {
+    Serializer<?> s = typeMap.get((byte) type);
+    if (s != null) {
+      return s.getBuilder();
+    }
+    return null;
+  }
 }
--- satviz-network/src/main/java/edu/kit/satviz/network/Receiver.java
+++ satviz-network/src/main/java/edu/kit/satviz/network/Receiver.java
@@ -1,22 +1,79 @@
 package edu.kit.satviz.network;
 
 import edu.kit.satviz.serial.SerialBuilder;
-
+import edu.kit.satviz.serial.SerializationException;
 import java.nio.ByteBuffer;
 import java.util.function.IntFunction;
 
+/**
+ * Transforms a byte stream into an object stream.
+ * Objects are deserialized based on given {@link edu.kit.satviz.serial.SerialBuilder}s.
+ *
+ * @author luwae
+ */
 public class Receiver {
   private final IntFunction<SerialBuilder<?>> gen;
+  private byte type;
+  private SerialBuilder<?> builder = null;
+  private NetworkObject netObj = null;
 
+  /**
+   * Creates a new empty receiver.
+   *
+   * @param gen a function generating {@link edu.kit.satviz.serial.SerialBuilder}s from a type
+   */
   public Receiver(IntFunction<SerialBuilder<?>> gen) {
     this.gen = gen;
   }
 
+  /**
+   * Receives input bytes from a bytebuffer and constructs objects.
+   * Reads bytes as long as the buffer is not empty and an object is not finished.
+   *
+   * @param bb the buffer to read from
+   * @return <code>true</code> if an object is present, <code>false</code> otherwise.
+   */
   public boolean receive(ByteBuffer bb) {
-    return false; // TODO
+    int nb = bb.remaining();
+    if (nb == 0) {
+      return false;
+    }
+
+    if (builder == null) {
+      type = bb.get();
+      builder = gen.apply(type);
+      nb--;
+    }
+
+    while (nb > 0) {
+      nb--;
+      boolean done;
+      try {
+        done = builder.addByte(bb.get());
+      } catch (SerializationException e) {
+        netObj = NetworkObject.createFail();
+        return true;
+      }
+
+      if (done) {
+        netObj = new NetworkObject(type, builder.getObject());
+        return true;
+      }
+    }
+    return false;
   }
 
+  /**
+   * Gets a finished network object.
+   *
+   * @return the network object (never <code>null</code>)
+   */
   public NetworkObject getObject() {
-    return null; // TODO
+    if (netObj != null) {
+      NetworkObject tmp = netObj;
+      netObj = null;
+      return tmp;
+    }
+    return NetworkObject.createNone();
   }
 }
