4d5329e9543129e16c7e7e7b84b7261e918d1ee4
LuWae <l.waeldele@t-online.de>
2022-01-18 15:41:44 +0100

    null serializer and builder to send nothing

--- satviz-network/src/main/java/edu/kit/satviz/network/NullSerializer.java
+++ satviz-network/src/main/java/edu/kit/satviz/network/NullSerializer.java
@@ -3,25 +3,41 @@ package edu.kit.satviz.network;
 import edu.kit.satviz.serial.SerialBuilder;
 import edu.kit.satviz.serial.SerializationException;
 import edu.kit.satviz.serial.Serializer;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * A {@link Serializer} for non-existent objects (<code>null</code>).
+ *
+ * @author luwae
+ */
 public class NullSerializer extends Serializer<Object> {
-  private boolean done = false;
 
+  /**
+   * Serializes a <code>null</code> object.
+   * It contains of a single null-byte.
+   *
+   * @param o ignored
+   * @param out the stream to write to
+   * @throws IOException if the stream can't be used
+   */
   @Override
-  public void serialize(Object o, OutputStream out) throws IOException, SerializationException {
+  public void serialize(Object o, OutputStream out) throws IOException {
     out.write(0);
   }
 
+  /**
+   * Deserializes a <code>null</code> object.
+   * Tries to read a single null-byte from the stream.
+   *
+   * @param in the stream to read from
+   * @return <code>null</code>
+   * @throws IOException if the stream can't be used
+   * @throws SerializationException if the stream contains invalid data
+   */
   @Override
   public Object deserialize(InputStream in) throws IOException, SerializationException {
-    if (done) {
-      throw new SerializationException("done");
-    }
-
     int i = in.read();
     if (i == -1) {
       throw new SerializationException("unexpected end of stream");
@@ -32,8 +48,14 @@ public class NullSerializer extends Serializer<Object> {
     return null;
   }
 
+  /**
+   * Returns a {@link SerialBuilder} for <code>null</code> objects.
+   * @see NullSerialBuilder
+   *
+   * @return new builder
+   */
   @Override
   public SerialBuilder<Object> getBuilder() {
-    return null;
+    return new NullSerialBuilder();
   }
 }
