7abbc5f9ebe5c81ea3011d325fff3c4a8bd6a0ef
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-18 10:26:07 +0100

    Add constraint system stubs

---
+++ satviz-common/src/main/java/edu/kit/satviz/common/FileExistsConstraint.java
@@ -0,0 +1,10 @@
+package edu.kit.satviz.common;
+
+import java.nio.file.Path;
+
+public class FileExistsConstraint implements Constraint<Path> {
+  @Override
+  public void validate(Path obj) throws ConstraintValidationException {
+
+  }
+}
