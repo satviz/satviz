0fa1801aaa41c1f50b73fa61c44297ef1b3d4e0e
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-19 12:20:14 +0100

    Document FileExistsConstraint

--- satviz-common/src/main/java/edu/kit/satviz/common/FileExistsConstraint.java
+++ satviz-common/src/main/java/edu/kit/satviz/common/FileExistsConstraint.java
@@ -3,10 +3,25 @@ package edu.kit.satviz.common;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/**
+ * A {@link Constraint} implementation that validates that a file at a given {@link Path} exists
+ * (or not).
+ *
+ * @see #fileExists()
+ * @see #fileDoesNotExist()
+ */
 public class FileExistsConstraint implements Constraint<Path> {
 
   private final boolean mustExist;
 
+  /**
+   * Constructor for this constraint type.
+   *
+   * @param mustExist If set to {@code true}, the constraint will fail if a file
+   *                  <em>does not exist</em>, if set to {@code false}, the opposite.
+   * @see #fileExists()
+   * @see #fileDoesNotExist()
+   */
   public FileExistsConstraint(boolean mustExist) {
     this.mustExist = mustExist;
   }
@@ -18,10 +33,21 @@ public class FileExistsConstraint implements Constraint<Path> {
     }
   }
 
+  /**
+   * Produces a version of this constraint that checks whether an input file <em>exists</em>.
+   *
+   * @return A constraint that fails if a given file does not exist.
+   */
   public static FileExistsConstraint fileExists() {
     return new FileExistsConstraint(true);
   }
 
+  /**
+   * Produces a version of this constraint that checks whether an input file
+   * <em>does not exist</em>.
+   *
+   * @return A constraint that fails if a given file exists.
+   */
   public static FileExistsConstraint fileDoesNotExist() {
     return new FileExistsConstraint(false);
   }
