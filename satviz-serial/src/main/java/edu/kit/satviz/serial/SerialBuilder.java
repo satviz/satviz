b17170e75ecefe3f7245f77bf31dd6a77e4409fd
LuWae <l.waeldele@t-online.de>
2022-01-19 20:01:19 +0100

    extended SerialBuilder stub interface

--- satviz-serial/src/main/java/edu/kit/satviz/serial/SerialBuilder.java
+++ satviz-serial/src/main/java/edu/kit/satviz/serial/SerialBuilder.java
@@ -1,8 +1,22 @@
 package edu.kit.satviz.serial;
 
+import java.nio.ByteBuffer;
+
 public abstract class SerialBuilder<T> {
 
   public abstract boolean addByte(int i) throws SerializationException;
+
+  public boolean addBytes(ByteBuffer bb) throws SerializationException {
+    // naive; @override for more efficient implementations
+    while (bb.hasRemaining()) {
+      if (addByte(bb.get())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public abstract boolean objectFinished();
 
   public abstract T getObject();
 }
