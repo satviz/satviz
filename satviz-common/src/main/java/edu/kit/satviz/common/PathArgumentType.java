6ea77c5eee952ff43cb33e7d625aec86af9793e9
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-19 14:17:24 +0100

    Implement PathArgumentType

--- satviz-common/src/main/java/edu/kit/satviz/common/PathArgumentType.java
+++ satviz-common/src/main/java/edu/kit/satviz/common/PathArgumentType.java
@@ -1,15 +1,22 @@
 package edu.kit.satviz.common;
 
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import net.sourceforge.argparse4j.inf.Argument;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.ArgumentType;
 
-import java.nio.file.Path;
-
 public class PathArgumentType implements ArgumentType<Path> {
+
   @Override
-  public Path convert(ArgumentParser parser, Argument arg, String value) throws ArgumentParserException {
-    return null;
+  public Path convert(ArgumentParser parser, Argument arg, String value)
+      throws ArgumentParserException {
+    try {
+      return Paths.get(value);
+    } catch (InvalidPathException e) {
+      throw new ArgumentParserException(e.getMessage(), e, parser);
+    }
   }
 }
