e6960cc395b9bf2e2510b776b8f0b473c861f0af
LuWae <l.waeldele@t-online.de>
2022-01-19 14:53:44 +0100

    reworked SatAssignment interface

--- satviz-sat/src/main/java/edu/kit/satviz/sat/SatAssignment.java
+++ satviz-sat/src/main/java/edu/kit/satviz/sat/SatAssignment.java
@@ -1,6 +1,28 @@
 package edu.kit.satviz.sat;
 
 public class SatAssignment {
+  public static enum VariableState {
+    DONTCARE(0),
+    SET(1),
+    UNSET(2),
+    RESERVED(3);
+
+    public final byte val;
+
+    VariableState(int val) {
+      this.val = (byte) val;
+    }
+
+    public static VariableState fromValue(int val) {
+      switch(val) {
+        case 0: return DONTCARE;
+        case 1: return SET;
+        case 2: return UNSET;
+        case 3: return RESERVED;
+      }
+      return null;
+    }
+  }
 
   private final int varCount;
   private final byte[] satAssignment;
@@ -8,29 +30,44 @@ public class SatAssignment {
   public SatAssignment(int varCount) {
     assert varCount > 0;
     this.varCount = varCount;
-    this.satAssignment = new byte[(varCount / 4) + ((varCount % 4 == 0) ? 0 : 1)];
+    this.satAssignment = new byte[(varCount + 4) >> 2];
   }
 
   /**
    * byte = 8 bits
    * => 4 different variables
    */
-  public void setVal(int variable, byte value) {
-    assert value < 4 && variable < this.varCount;
-    int shift = 2 * (variable % 4);
+  public void set(int variable, VariableState state) {
+    if (variable <= 0 || variable > varCount) {
+      return;
+    }
+
+    int shift = (variable & 3) << 1;
     byte mask = (byte) ~(3 << shift);
 
     byte result = (byte) (
-            (value << shift) | (this.satAssignment[(variable / 4)] & mask)
+            (state.val << shift) | (this.satAssignment[(variable >> 2)] & mask)
     );
-    this.satAssignment[(variable / 4)] = result;
+    this.satAssignment[(variable >> 2)] = result;
   }
 
-  public int getVal(int variable) {
-    assert variable < this.varCount;
-    int shift = 2 * (variable % 4);
+  public VariableState get(int variable) {
+    if (variable <= 0 || variable > varCount) {
+      return VariableState.DONTCARE;
+    }
 
-    return (this.satAssignment[variable / 4] >> shift) & 3;
+    int shift = (variable & 3) << 1;
+
+    return VariableState.fromValue((this.satAssignment[variable >> 2] >> shift) & 3);
+  }
+
+  public int getIntState(int variable) {
+    VariableState state = get(variable);
+    return switch (state) {
+      case SET -> variable;
+      case UNSET -> -variable;
+      default -> 0;
+    };
   }
 
   public int getVarCount() {
