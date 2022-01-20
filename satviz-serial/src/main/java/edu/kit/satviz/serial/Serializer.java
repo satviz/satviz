34d32db639744afe95a877d452b547bbcadf744f
LuWae <l.waeldele@t-online.de>
2022-01-20 18:06:40 +0100

    unsafe serialization with ClassCastException

--- satviz-serial/src/main/java/edu/kit/satviz/serial/Serializer.java
+++ satviz-serial/src/main/java/edu/kit/satviz/serial/Serializer.java
@@ -8,6 +8,13 @@ public abstract class Serializer<T> {
 
   public abstract void serialize(T t, OutputStream out) throws IOException, SerializationException;
 
+  public void serializeUnsafe(Object o, OutputStream out) throws IOException,
+      ClassCastException, SerializationException {
+    @SuppressWarnings("unchecked")
+    T t = (T) o;
+    serialize(t, out);
+  }
+
   public abstract T deserialize(InputStream in) throws IOException, SerializationException;
 
   public abstract SerialBuilder<T> getBuilder();
