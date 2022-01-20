472033f552b952dfa30efa13b2a5daa1b4b11a36
Kevin Kuryshev <kuryshevk@gmail.com>
2022-01-20 14:25:41 +0100

    Added equals, hashCode and toString methods to the Clause record

--- satviz-sat/src/main/java/edu/kit/satviz/sat/Clause.java
+++ satviz-sat/src/main/java/edu/kit/satviz/sat/Clause.java
@@ -1,13 +1,38 @@
 package edu.kit.satviz.sat;
 
+import java.util.Arrays;
+
 /**
  * A simple record class, that stores the literals of a clause in an integer array.<br>
- * For this to work, every variable of the SAT-Instance has to have an identifiable <code>varID</code>.
- * In case that variable within the clause is used as a positive literal,
+ * For this to work, every variable of the SAT-Instance has to have an identifiable
+ * <code>varID</code>. In case that variable within the clause is used as a positive literal,
  * the integer stored for it will be <code>varID</code>, and if it's used as a negative literal,
  * then the integer stored will be <code>-varID</code>.
  *
  * @author quorty
  */
 public record Clause(int[] literals) {
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Clause clause = (Clause) o;
+    return Arrays.equals(literals, clause.literals);
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(literals);
+  }
+
+  @Override
+  public String toString() {
+    return Arrays.toString(literals);
+  }
+
 }
