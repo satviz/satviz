070c2c5116fdb32887338ee5a400bddbc084174e
Kevin Kuryshev <kuryshevk@gmail.com>
2022-01-21 16:55:34 +0100

    Added dependency to satviz-sat module (so that we can serialize the classes from there)

--- satviz-serial/build.gradle.kts
+++ satviz-serial/build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     `java-library`
 }
+
+repositories {
+    maven("https://repo.devcord.club/snapshots")
+}
+
+dependencies {
+    implementation("edu.kit.satviz:ipasir4j:0.1.0-SNAPSHOT")
+    implementation(project(":satviz-sat"))
+}
