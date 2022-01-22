d5148d5372ce047fd86ab0f07afb0a1d0255f86b
LuWae <l.waeldele@t-online.de>
2022-01-22 23:14:58 +0100

    byte array streams for testing

---
+++ satviz-serial/src/test/java/edu/kit/satviz/serial/ByteArrayInputStream.java
@@ -0,0 +1,20 @@
+package edu.kit.satviz.serial;
+
+import java.io.InputStream;
+
+public class ByteArrayInputStream extends InputStream {
+  private final byte[] b;
+  private int readpos = 0;
+
+  public ByteArrayInputStream(byte[] b) {
+    this.b = b;
+  }
+
+  @Override
+  public int read() {
+    if (readpos == b.length) {
+      return -1;
+    }
+    return b[readpos++];
+  }
+}
---
+++ satviz-serial/src/test/java/edu/kit/satviz/serial/ByteArrayOutputStream.java
@@ -0,0 +1,21 @@
+package edu.kit.satviz.serial;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ByteArrayOutputStream extends OutputStream {
+  private final byte[] b;
+  private int writepos = 0;
+
+  public ByteArrayOutputStream(byte[] b) {
+    this.b = b;
+  }
+
+  @Override
+  public void write(int i) throws IOException {
+    if (writepos == b.length) {
+      throw new IOException("stream finished");
+    }
+    b[writepos++] = (byte) i;
+  }
+}
