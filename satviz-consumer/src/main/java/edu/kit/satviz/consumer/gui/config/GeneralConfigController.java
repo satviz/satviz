58ab9b77494f65ea45aa5328b20ba0ca2acdba8e
Tim-17 <tim@holzenkamp.eu>
2022-01-23 18:14:48 +0100

    add attributes for FXML components

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/config/GeneralConfigController.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/config/GeneralConfigController.java
@@ -1,6 +1,45 @@
 package edu.kit.satviz.consumer.gui.config;
 
+import edu.kit.satviz.consumer.config.ConsumerMode;
+import edu.kit.satviz.consumer.config.WeightFactor;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+
 public class GeneralConfigController extends ConfigController {
+
+  @FXML
+  private Button loadSettingsButton;
+  @FXML
+  private Button saveSettingsButton;
+  @FXML
+  private Button recordingFileButton;
+  @FXML
+  private Label recordingFileLabel;
+  @FXML
+  private CheckBox showLiveVisualizationCheckBox;
+  @FXML
+  private CheckBox recordFromStartCheckBox;
+  @FXML
+  private ChoiceBox<WeightFactor> weightFactorChoiceBox;
+  @FXML
+  private Spinner<Integer> windowSizeSpinner;
+  @FXML
+  private ColorPicker coldColorColorPicker;
+  @FXML
+  private ColorPicker hotColorColorPicker;
+  @FXML
+  private Button satInstanceFileButton;
+  @FXML
+  private Label satInstanceFileLabel;
+  @FXML
+  private ChoiceBox<ConsumerMode> modeChoiceBox;
+  @FXML
+  private Button runButton;
 
   @Override
   protected void run() {
