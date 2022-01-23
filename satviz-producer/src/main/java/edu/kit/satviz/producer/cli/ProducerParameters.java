1d3d7fde4a28aa434cbbfbc2810342fd1fd345b7
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-23 18:01:06 +0100

    Implement ProducerParameters structure

--- satviz-producer/src/main/java/edu/kit/satviz/producer/cli/ProducerParameters.java
+++ satviz-producer/src/main/java/edu/kit/satviz/producer/cli/ProducerParameters.java
@@ -1,4 +1,61 @@
 package edu.kit.satviz.producer.cli;
 
-public class ProducerParameters {
+import java.nio.file.Path;
+
+public final class ProducerParameters {
+
+  private Path instanceFile;
+  private Path solverFile;
+  private Path proofFile;
+  private int port;
+  private String host;
+  private boolean noWait;
+
+  public Path getInstanceFile() {
+    return instanceFile;
+  }
+
+  public Path getSolverFile() {
+    return solverFile;
+  }
+
+  public Path getProofFile() {
+    return proofFile;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public String getHost() {
+    return host;
+  }
+
+  public boolean isNoWait() {
+    return noWait;
+  }
+
+  public void setInstanceFile(Path instanceFile) {
+    this.instanceFile = instanceFile;
+  }
+
+  public void setSolverFile(Path solverFile) {
+    this.solverFile = solverFile;
+  }
+
+  public void setProofFile(Path proofFile) {
+    this.proofFile = proofFile;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+
+  public void setNoWait(boolean noWait) {
+    this.noWait = noWait;
+  }
 }
