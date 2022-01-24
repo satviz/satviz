d4fb162568bb937b4887f73e14f688a1c66006f1
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-24 20:37:10 +0100

    Add constructor to ProofSource

--- satviz-producer/src/main/java/edu/kit/satviz/producer/source/ProofSource.java
+++ satviz-producer/src/main/java/edu/kit/satviz/producer/source/ProofSource.java
@@ -2,8 +2,18 @@ package edu.kit.satviz.producer.source;
 
 import edu.kit.satviz.producer.ClauseSource;
 import edu.kit.satviz.producer.SourceOpeningException;
+import edu.kit.satviz.sat.ClauseUpdate;
+
+import java.util.Iterator;
 
 public class ProofSource extends ClauseSource {
+
+  private final Iterator<ClauseUpdate> updates;
+
+  public ProofSource(Iterator<ClauseUpdate> updates) {
+    this.updates = updates;
+  }
+
   @Override
   public void open() throws SourceOpeningException {
 
