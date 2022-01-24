d9cdbd26c8da753a726c5e190ef95409150852eb
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-24 15:34:14 +0100

    Begin implementing ConnectionListener

--- satviz-producer/src/main/java/edu/kit/satviz/producer/ConnectionListener.java
+++ satviz-producer/src/main/java/edu/kit/satviz/producer/ConnectionListener.java
@@ -1,15 +1,47 @@
 package edu.kit.satviz.producer;
 
+import edu.kit.satviz.network.ProducerConnection;
 import edu.kit.satviz.network.ProducerConnectionListener;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ConnectionListener implements ProducerConnectionListener {
+
+  private static final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+  private final ProducerConnection connection;
+  private final ClauseSource source;
+
+  public ConnectionListener(ProducerConnection connection, ClauseSource source) {
+    this.connection = connection;
+    this.source = source;
+    source.subscribe(connection::sendClauseUpdate);
+    source.whenDone(connection::terminateSolved);
+    source.whenClosed(connection::terminateRefuted);
+  }
+
   @Override
   public void onConnect() {
+    executor.submit(() -> {
+      try (source) {
+        source.open();
+      } catch (SourceOpeningException e) {
+        e.printStackTrace();
+        connection.terminateFailed(e.getMessage());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
 
   }
 
   @Override
   public void onDisconnect(String reason) {
-
+    try {
+      source.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
