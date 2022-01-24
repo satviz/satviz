1c88183d06db01940ec6adefe37705d39a3615ba
Tim-17 <tim@holzenkamp.eu>
2022-01-24 10:53:20 +0100

    implement ConfigStarter

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/config/ConfigStarter.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/config/ConfigStarter.java
@@ -2,17 +2,30 @@ package edu.kit.satviz.consumer.gui.config;
 
 import edu.kit.satviz.consumer.config.ConsumerConfig;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ConfigStarter extends Application {
 
+  private static GeneralConfigController configController;
+
   @Override
   public void start(Stage primaryStage) throws Exception {
+    configController = new GeneralConfigController();
 
+    FXMLLoader fxmlLoader = new FXMLLoader(ConfigStarter.class.getResource("general-config.fxml"));
+    fxmlLoader.setController(configController);
+
+    Scene scene = new Scene(fxmlLoader.load());
+    primaryStage.setTitle("Configuration");
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
+    primaryStage.show();
   }
 
   public static ConsumerConfig getConsumerConfig() {
-    return null;
+    return configController.getConsumerConfig();
   }
 
 }
