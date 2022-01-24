8e8050ec722b5225ba3d3bbc322c5b79898fb1ae
Tim-17 <tim@holzenkamp.eu>
2022-01-24 17:28:01 +0100

    add attributes & stubs for methods for FXML components

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/config/ExternalConfigController.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/config/ExternalConfigController.java
@@ -1,6 +1,17 @@
 package edu.kit.satviz.consumer.gui.config;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Spinner;
+
 public class ExternalConfigController extends ConfigController {
+
+  @FXML
+  private Spinner<Integer> portSpinner;
+
+  @FXML
+  private void setPort() {
+
+  }
 
   @Override
   protected void run() {
