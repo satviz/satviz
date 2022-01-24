8d0a371b696fe3637a1c533fb90a0565eebe6215
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-24 20:32:48 +0100

    Add constructors to SourceOpeningException

--- satviz-producer/src/main/java/edu/kit/satviz/producer/SourceOpeningException.java
+++ satviz-producer/src/main/java/edu/kit/satviz/producer/SourceOpeningException.java
@@ -1,4 +1,16 @@
 package edu.kit.satviz.producer;
 
 public class SourceOpeningException extends Exception {
+
+  public SourceOpeningException(String message) {
+    super(message);
+  }
+
+  public SourceOpeningException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public SourceOpeningException(Throwable cause) {
+    super(cause);
+  }
 }
