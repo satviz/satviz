21fb9bbd20f9de40c0614b3c4117a4f636695585
Tim-17 <tim@holzenkamp.eu>
2022-01-24 11:25:34 +0100

    add attributes for FXML components

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/config/EmbeddedConfigController.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/config/EmbeddedConfigController.java
@@ -1,6 +1,19 @@
 package edu.kit.satviz.consumer.gui.config;
 
+import edu.kit.satviz.consumer.config.EmbeddedModeSource;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
 public class EmbeddedConfigController extends ConfigController {
+
+  @FXML
+  private ChoiceBox<EmbeddedModeSource> producerModeChoiceBox;
+  @FXML
+  private Button producerModeFileButton;
+  @FXML
+  private Label producerModeFileLabel;
 
   @Override
   protected void run() {
