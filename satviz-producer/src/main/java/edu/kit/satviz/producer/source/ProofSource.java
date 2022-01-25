40b2db7cc5166f1d46b5d0405e9cda71fb04e04b
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-25 11:41:29 +0100

    Implement ProofSource

--- satviz-producer/src/main/java/edu/kit/satviz/producer/source/ProofSource.java
+++ satviz-producer/src/main/java/edu/kit/satviz/producer/source/ProofSource.java
@@ -1,24 +1,39 @@
 package edu.kit.satviz.producer.source;
 
 import edu.kit.satviz.parsers.DratFile;
+import edu.kit.satviz.parsers.ParsingException;
 import edu.kit.satviz.producer.ClauseSource;
 import edu.kit.satviz.producer.SourceException;
+import edu.kit.satviz.sat.ClauseUpdate;
+import java.io.IOException;
 
 public class ProofSource extends ClauseSource {
 
-  private final DratFile drat;
+  private final DratFile proof;
 
-  public ProofSource(DratFile drat) {
-    this.drat = drat;
+  private volatile boolean stop;
+
+  public ProofSource(DratFile proof) {
+    this.proof = proof;
+    this.stop = false;
   }
 
   @Override
   public void open() throws SourceException {
-
+    try (proof) {
+      for (ClauseUpdate update : proof) {
+        clauseListener.accept(update);
+      }
+    } catch (ParsingException e) {
+      throw new SourceException("DRAT proof parsing error", e);
+    } catch (IOException e) {
+      throw new SourceException("DRAT proof I/O error", e);
+    }
+    refutedListener.run();
   }
 
   @Override
-  public void close() throws Exception {
-
+  public void close() {
+    stop = true;
   }
 }
