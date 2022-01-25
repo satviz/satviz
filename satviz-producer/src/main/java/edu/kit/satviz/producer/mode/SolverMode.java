0d02f9948fe35dc5a6ade175b682b7bcc65f13e3
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-25 10:55:57 +0100

    Use close method on instance file in solver mode

--- satviz-producer/src/main/java/edu/kit/satviz/producer/mode/SolverMode.java
+++ satviz-producer/src/main/java/edu/kit/satviz/producer/mode/SolverMode.java
@@ -3,14 +3,13 @@ package edu.kit.satviz.producer.mode;
 import edu.kit.ipasir4j.Ipasir;
 import edu.kit.ipasir4j.Solver;
 import edu.kit.satviz.parsers.DimacsFile;
+import edu.kit.satviz.parsers.ParsingException;
 import edu.kit.satviz.producer.ClauseSource;
 import edu.kit.satviz.producer.ProducerMode;
 import edu.kit.satviz.producer.SourceException;
 import edu.kit.satviz.producer.cli.ProducerParameters;
 import edu.kit.satviz.producer.source.SolverSource;
-import edu.kit.satviz.sat.Clause;
 import edu.kit.satviz.sat.ClauseUpdate;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,10 +23,16 @@ public class SolverMode implements ProducerMode {
   @Override
   public ClauseSource createSource(ProducerParameters parameters) throws SourceException {
     tryLoadSolver(parameters.getSolverFile());
-    DimacsFile dimacs = tryOpenInstance(parameters.getInstanceFile());
-    Solver solver = Ipasir.init();
-    configureSolver(solver, dimacs);
-    return new SolverSource(solver, dimacs.getVariableAmount());
+    try (DimacsFile instance = new DimacsFile(Files.newInputStream(parameters.getInstanceFile()))) {
+      Solver solver = Ipasir.init();
+      configureSolver(solver, instance);
+      return new SolverSource(solver, instance.getVariableAmount());
+    } catch (IOException e) {
+      throw new SourceException("I/O exception trying to read instance file", e);
+    } catch (ParsingException e) {
+      throw new SourceException("Error while parsing DIMACS CNF file", e);
+    }
+
   }
 
   private void tryLoadSolver(Path path) throws SourceException {
@@ -35,14 +40,6 @@ public class SolverMode implements ProducerMode {
       System.load(path.toAbsolutePath().toString());
     } catch (UnsatisfiedLinkError e) {
       throw new SourceException("Could not load solver shared library", e);
-    }
-  }
-
-  private DimacsFile tryOpenInstance(Path path) throws SourceException {
-    try {
-      return new DimacsFile(Files.newInputStream(path));
-    } catch (IOException e) {
-      throw new SourceException("Could not open instance file", e);
     }
   }
 
