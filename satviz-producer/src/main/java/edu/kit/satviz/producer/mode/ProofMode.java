a4cfdfd7b6a0f154aabab2f58852907555267ca4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-25 10:33:20 +0100

    Implement ProofMode

--- satviz-producer/src/main/java/edu/kit/satviz/producer/mode/ProofMode.java
+++ satviz-producer/src/main/java/edu/kit/satviz/producer/mode/ProofMode.java
@@ -1,8 +1,15 @@
 package edu.kit.satviz.producer.mode;
 
+import edu.kit.satviz.parsers.DratFile;
 import edu.kit.satviz.producer.ClauseSource;
 import edu.kit.satviz.producer.ProducerMode;
+import edu.kit.satviz.producer.SourceException;
 import edu.kit.satviz.producer.cli.ProducerParameters;
+import edu.kit.satviz.producer.source.ProofSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 public class ProofMode implements ProducerMode {
   @Override
@@ -11,7 +18,13 @@ public class ProofMode implements ProducerMode {
   }
 
   @Override
-  public ClauseSource createSource(ProducerParameters parameters) {
-    return null;
+  public ClauseSource createSource(ProducerParameters parameters) throws SourceException {
+    try {
+      InputStream proofStream = Files.newInputStream(parameters.getProofFile());
+      DratFile drat = new DratFile(proofStream);
+      return new ProofSource(drat);
+    } catch (IOException e) {
+      throw new SourceException("Could not create source - I/O error", e);
+    }
   }
 }
