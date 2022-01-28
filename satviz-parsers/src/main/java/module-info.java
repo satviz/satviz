52e5699eb44bc94a8d37d16475d7fe0a7dd8a706
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-28 11:59:47 +0100

    Put module-info back in the right place

--- satviz-parsers/src/main/java/module-info.java
+++ satviz-parsers/src/main/java/module-info.java
@@ -2,4 +2,6 @@ module edu.kit.satviz.parsers {
 
   requires transitive edu.kit.satviz.sat;
 
+  exports edu.kit.satviz.parsers;
+
 }
--- satviz-producer/src/main/java/module-info.java
+++ satviz-producer/src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module edu.kit.satviz.producer {
 
   requires edu.kit.satviz.common;
+  requires edu.kit.satviz.sat;
+  requires edu.kit.satviz.parsers;
+  requires edu.kit.satviz.network;
   requires edu.kit.ipasir4j;
   requires net.sourceforge.argparse4j;
 
   opens edu.kit.satviz.producer.cli to net.sourceforge.argparse4j;
+  opens edu.kit.satviz.producer.source to edu.kit.ipasir4j;
 }
