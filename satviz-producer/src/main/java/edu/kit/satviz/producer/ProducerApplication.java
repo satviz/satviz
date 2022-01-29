fc891abb293e3726aef31003823d038d1ba7647c
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-29 18:37:27 +0100

    Continue ProducerApplication implementation

--- satviz-producer/src/main/java/edu/kit/satviz/producer/ProducerApplication.java
+++ satviz-producer/src/main/java/edu/kit/satviz/producer/ProducerApplication.java
@@ -2,6 +2,8 @@ package edu.kit.satviz.producer;
 
 import edu.kit.satviz.common.Constraint;
 import edu.kit.satviz.common.ConstraintValidationException;
+import edu.kit.satviz.network.ProducerConnection;
+import edu.kit.satviz.network.ProducerId;
 import edu.kit.satviz.producer.cli.ProducerCli;
 import edu.kit.satviz.producer.cli.ProducerConstraints;
 import edu.kit.satviz.producer.cli.ProducerParameters;
@@ -13,28 +15,51 @@ import java.util.List;
 
 public class ProducerApplication {
 
+  private static final List<ProducerMode> SUPPORTED_MODES
+      = List.of(new ProofMode(), new SolverMode());
+
   public static void main(String[] args) {
-    List<ProducerMode> supportedModes = List.of(new ProofMode(), new SolverMode());
-    Constraint<ProducerParameters> inputConstraint
-        = ProducerConstraints.paramConstraints(supportedModes);
 
-    ProducerParameters parameters;
-    try {
-      parameters = ProducerCli.parseArgs(args);
-    } catch (ArgumentParserException e) {
-      ProducerCli.PARSER.handleError(e);
-      System.exit(1);
-      return;
-    }
+    ProducerParameters parameters = parseArgs(args);
+    validateArgs(parameters);
+    ProducerMode selectedMode = SUPPORTED_MODES.stream()
+        .filter(mode -> mode.isSet(parameters))
+        .findFirst()
+        .orElseThrow();
 
     try {
-      inputConstraint.validate(parameters);
-    } catch (ConstraintValidationException e) {
+      ClauseSource source = selectedMode.createSource(parameters);
+      ProducerConnection connection = new ProducerConnection(); // TODO: 29/01/2022 host, port
+      connection.register(new ConnectionListener(connection, source));
+      // TODO: 29/01/2022
+      connection.establish(new ProducerId(null, null, null, false, 0));
+    } catch (SourceException e) {
       System.err.println(e.getMessage());
       System.exit(1);
-      return;
     }
 
   }
+
+  private static ProducerParameters parseArgs(String[] args) {
+    try {
+      return ProducerCli.parseArgs(args);
+    } catch (ArgumentParserException e) {
+      ProducerCli.PARSER.handleError(e);
+      System.exit(1);
+      return null;
+    }
+  }
+
+  private static void validateArgs(ProducerParameters params) {
+    Constraint<ProducerParameters> inputConstraint
+        = ProducerConstraints.paramConstraints(SUPPORTED_MODES);
+    try {
+      inputConstraint.validate(params);
+    } catch (ConstraintValidationException e) {
+      System.err.println(e.getMessage());
+      System.exit(1);
+    }
+  }
+
 
 }
