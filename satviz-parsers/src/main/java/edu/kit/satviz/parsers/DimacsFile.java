86a96b9cf77bccfb695579d34c6374bb84181ad5
Kevin Kuryshev <kuryshevk@gmail.com>
2022-01-31 17:06:45 +0100

    Fixed bug, in which the header couldn't get read properly

--- satviz-parsers/src/main/java/edu/kit/satviz/parsers/DimacsFile.java
+++ satviz-parsers/src/main/java/edu/kit/satviz/parsers/DimacsFile.java
@@ -3,6 +3,7 @@ package edu.kit.satviz.parsers;
 import edu.kit.satviz.sat.ClauseUpdate;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -26,7 +27,12 @@ public class DimacsFile implements Iterable<ClauseUpdate>, AutoCloseable {
     do {
       header = scanner.next();
     } while (header.matches("p cnf (([1-9][0-9]*)|0) (([1-9][0-9]*)|0)"));
-    String[] numberStrings = header.replaceAll("[\\D]", " ").trim().split(" ");
+    String[] numberStrings = header.replaceAll("[\\D]", " ").split(" ");
+    numberStrings = Arrays.stream(numberStrings)
+            .filter(value ->
+                    value != null && value.length() > 0
+            )
+            .toArray(String[]::new);
     variableAmount = Integer.parseInt(numberStrings[0]);
     clauseAmount = Integer.parseInt(numberStrings[1]);
   }
