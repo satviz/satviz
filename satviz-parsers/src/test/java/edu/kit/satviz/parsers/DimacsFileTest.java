3fd6174d376527aee333561ddf21a88c5f89cbb9
Kevin Kuryshev <kuryshevk@gmail.com>
2022-01-31 16:34:13 +0100

    Added 2 sample cnf files (that i got from https://people.sc.fsu.edu/~jburkardt/data/cnf/cnf.html) and created an empty test-class for the DimacsFile class

---
+++ satviz-parsers/src/test/java/edu/kit/satviz/parsers/DimacsFileTest.java
@@ -0,0 +1,20 @@
+package edu.kit.satviz.parsers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DimacsFileTest {
+
+
+
+  @BeforeEach
+  void setUp() {
+
+  }
+
+  @Test
+  void test() {
+
+  }
+
+}
