c2be9ef4076b18a9821f376b1e627f892c704003
Kevin Kuryshev <kuryshevk@gmail.com>
2022-01-31 14:50:00 +0100

    Full implementation of DimacsFile class

--- satviz-parsers/src/main/java/edu/kit/satviz/parsers/DimacsFile.java
+++ satviz-parsers/src/main/java/edu/kit/satviz/parsers/DimacsFile.java
@@ -1,32 +1,52 @@
 package edu.kit.satviz.parsers;
 
 import edu.kit.satviz.sat.ClauseUpdate;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class DimacsFile implements Iterable<ClauseUpdate>, AutoCloseable {
 
-  public DimacsFile(InputStream in) {
+  private final InputStream in;
+  private final ClauseParsingIterator parsingIterator;
+  private int variableAmount;
+  private int clauseAmount;
 
+  public DimacsFile(InputStream in) {
+    this.in = in;
+    Scanner scanner = new Scanner(in);
+    scanner.useDelimiter("\n");
+    parseHeader(scanner);
+    parsingIterator = new DimacsParsingIterator(scanner);
+  }
+
+  private void parseHeader(Scanner scanner) {
+    String header;
+    do {
+      header = scanner.next();
+    } while (header.matches("p cnf (([1-9][0-9]*)|0) (([1-9][0-9]*)|0)"));
+    String[] numberStrings = header.replaceAll("[\\D]", " ").trim().split(" ");
+    variableAmount = Integer.parseInt(numberStrings[0]);
+    clauseAmount = Integer.parseInt(numberStrings[1]);
   }
 
   @Override
   public Iterator<ClauseUpdate> iterator() {
-    return null;
+    return parsingIterator;
   }
 
   public int getVariableAmount() {
-    return 0;
+    return variableAmount;
   }
 
   public int getClauseAmount() {
-    return 0;
+    return clauseAmount;
   }
 
   @Override
   public void close() throws IOException {
-
+    in.close();
   }
+
 }
