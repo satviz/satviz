90971b32c3257a74be43c361c041c8664bbf865a
Kevin Kuryshev <kuryshevk@gmail.com>
2022-01-31 17:03:38 +0100

    Added 2 tests to DimacsFileTest

--- satviz-parsers/src/test/java/edu/kit/satviz/parsers/DimacsFileTest.java
+++ satviz-parsers/src/test/java/edu/kit/satviz/parsers/DimacsFileTest.java
@@ -2,19 +2,29 @@ package edu.kit.satviz.parsers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DimacsFileTest {
 
-
+  InputStream in;
 
   @BeforeEach
   void setUp() {
-
+    in = DimacsFileTest.class.getResourceAsStream("/dimacs_ex/simple_v3_c2.cnf");
   }
 
   @Test
-  void test() {
+  void getVariableAmount_test() {
+    DimacsFile simpleFile = new DimacsFile(in);
+    assertEquals(3, simpleFile.getVariableAmount());
+  }
 
+  @Test
+  void getClauseAmount_test() {
+    DimacsFile simpleFile = new DimacsFile(in);
+    assertEquals(2, simpleFile.getClauseAmount());
   }
 
 }
