27c77f928c23c9862adaef57809a50e076540406
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-05 19:14:38 +0100

    Implement more Java side Graph bindings

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/bindings/Graph.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/bindings/Graph.java
@@ -2,9 +2,7 @@ package edu.kit.satviz.consumer.bindings;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodType;
-import jdk.incubator.foreign.CLinker;
-import jdk.incubator.foreign.FunctionDescriptor;
-import jdk.incubator.foreign.MemoryAddress;
+import jdk.incubator.foreign.*;
 
 public class Graph extends NativeObject {
 
@@ -20,6 +18,41 @@ public class Graph extends NativeObject {
       FunctionDescriptor.ofVoid(CLinker.C_POINTER)
   );
 
+  private static final MethodHandle SUBMIT_WEIGHT_UPDATE = lookupFunction(
+      "submit_weight_update",
+      MethodType.methodType(void.class, MemoryAddress.class, MemoryAddress.class),
+      FunctionDescriptor.ofVoid(CLinker.C_POINTER, CLinker.C_POINTER)
+  );
+
+  private static final MethodHandle SUBMIT_HEAT_UPDATE = lookupFunction(
+      "submit_heat_update",
+      MethodType.methodType(void.class, MemoryAddress.class, MemoryAddress.class),
+      FunctionDescriptor.ofVoid(CLinker.C_POINTER, CLinker.C_POINTER)
+  );
+
+  private static final MethodHandle RECALCULATE_LAYOUT = lookupFunction(
+      "recalculate_layout",
+      MethodType.methodType(void.class, MemoryAddress.class),
+      FunctionDescriptor.ofVoid(CLinker.C_POINTER)
+  );
+
+  private static final MethodHandle ADAPT_LAYOUT = lookupFunction(
+      "adapt_layout",
+      MethodType.methodType(void.class, MemoryAddress.class),
+      FunctionDescriptor.ofVoid(CLinker.C_POINTER)
+  );
+
+  private static final MethodHandle SERIALIZE = lookupFunction(
+      "serialize",
+      MethodType.methodType(MemoryAddress.class, MemoryAddress.class),
+      FunctionDescriptor.of(CLinker.C_POINTER, CLinker.C_POINTER)
+  );
+
+  private static final MethodHandle DESERIALIZE = lookupFunction(
+      "deserialize",
+      MethodType.methodType(void.class, MemoryAddress.class, MemoryAddress.class),
+      FunctionDescriptor.ofVoid(CLinker.C_POINTER, CLinker.C_POINTER)
+  );
 
   private Graph(MemoryAddress pointer) {
     super(pointer);
@@ -33,10 +66,43 @@ public class Graph extends NativeObject {
     }
   }
 
+  public void recalculateLayout() {
+    try {
+      RECALCULATE_LAYOUT.invokeExact(pointer);
+    } catch (Throwable e) {
+      throw new NativeInvocationException("Error while recalculating layout", e);
+    }
+  }
+
+  public void adaptLayout() {
+    try {
+      ADAPT_LAYOUT.invokeExact(pointer);
+    } catch (Throwable e) {
+      throw new NativeInvocationException("Error while adapting layout", e);
+    }
+  }
+
+  public String serialize() {
+    try {
+      return CLinker.toJavaString((MemoryAddress) SERIALIZE.invokeExact(pointer));
+    } catch (Throwable e) {
+      throw new NativeInvocationException("Error while serializing graph", e);
+    }
+  }
+
+  public void deserialize(String string) {
+    try (ResourceScope local = ResourceScope.newConfinedScope()) {
+      DESERIALIZE.invokeExact(pointer, CLinker.toCString(string, local));
+    } catch (Throwable e) {
+      throw new NativeInvocationException("Error while deserializing graph representation", e);
+    }
+
+  }
+
   @Override
   public void close() {
     try {
-      RELEASE_GRAPH.invokeExact();
+      RELEASE_GRAPH.invokeExact(pointer);
     } catch (Throwable e) {
       throw new NativeInvocationException("Error while releasing graph", e);
     }
