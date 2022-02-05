d242a19a2cf28b7b68296e8bfd40dfebade9e5bb
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-05 12:38:02 +0100

    Add layout functions

--- satviz-consumer-native/src/satviz/Bindings.cpp
+++ satviz-consumer-native/src/satviz/Bindings.cpp
@@ -14,4 +14,14 @@ void satviz_release_graph(void *graph) {
   delete (Graph*) graph;
 }
 
+void satviz_recalculate_layout(void *graph) {
+  reinterpret_cast<Graph*>(graph)->recalculateLayout();
+}
+
+void satviz_adapt_layout(void *graph) {
+  //reinterpret_cast<Graph*>(graph)->adaptLayout(); // TODO not implemented yet
+}
+
+
+
 }
