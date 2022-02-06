fd0f210eae0a320a1af95e945daeeaa02ea7e130
Kevin Kuryshev <kuryshevk@gmail.com>
2022-02-06 13:10:17 +0100

    Added javadoc to ParsingException

--- satviz-parsers/src/main/java/edu/kit/satviz/parsers/ParsingException.java
+++ satviz-parsers/src/main/java/edu/kit/satviz/parsers/ParsingException.java
@@ -1,7 +1,16 @@
 package edu.kit.satviz.parsers;
 
+/**
+ * This exception is thrown, whenever the file that's currently parsed is either
+ * syntactically incorrect or contains invalid data.
+ */
 public class ParsingException extends RuntimeException {
 
+  /**
+   * This constructor creates an instance of the <code>ParsingException</code> class.
+   *
+   * @param message The error message that should be displayed.
+   */
   public ParsingException(String message) {
     super(message);
   }
