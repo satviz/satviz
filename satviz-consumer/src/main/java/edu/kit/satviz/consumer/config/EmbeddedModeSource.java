827b27203579250ce172a3e22472af5505feffa8
Tim-17 <tim@holzenkamp.eu>
2022-02-07 17:05:42 +0100

    add list of file extensions to enum values

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/config/EmbeddedModeSource.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/config/EmbeddedModeSource.java
@@ -1,14 +1,41 @@
 package edu.kit.satviz.consumer.config;
 
+import java.util.List;
+
 /**
  * This enum allows for different types of sources for the embedded
  * producer to be differentiated, such as a solver file or a finished
  * proof as a DRAT-file.
  *
  * @author johnnyjayjay
+ * @author Tim-17
  */
 public enum EmbeddedModeSource {
 
-  SOLVER, PROOF
+  /**
+   * Use a SAT solver as the source.
+   */
+  SOLVER(List.of(".so")),
+
+  /**
+   * Use a finished proof as the source.
+   */
+  PROOF(List.of(".drat"));
+
+  private final List<String> fileExtensions;
+
+  EmbeddedModeSource(List<String> fileExtensions) {
+    this.fileExtensions = fileExtensions;
+  }
+
+  /**
+   * Returns a list of all the file extensions that a source file of the type
+   * specified by the given enum value may use.
+   *
+   * @return the list of file extensions
+   */
+  public List<String> getFileExtensions() {
+    return fileExtensions;
+  }
 
 }
