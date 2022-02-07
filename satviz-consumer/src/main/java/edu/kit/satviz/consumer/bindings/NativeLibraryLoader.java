d9e8173e3815b4aac24ba5029b3c9ab9974c83dd
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-07 12:05:37 +0100

    Document NativeLibraryLoader

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/bindings/NativeLibraryLoader.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/bindings/NativeLibraryLoader.java
@@ -6,12 +6,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
+/**
+ * Utility class to load shared libraries from the resources of this application.
+ */
 public final class NativeLibraryLoader {
 
   private NativeLibraryLoader() {
 
   }
 
+  /**
+   * Loads a shared library located at the given resource path by copying it to a temporary file
+   * and using {@link System#load(String)}.
+   *
+   * @param path The path to the shared library file.
+   *             Relative resource paths will be resolved from this class' package.
+   * @throws IOException If the resource can't be copied to an actual file.
+   */
   public static void loadLibrary(String path) throws IOException {
     Path file = Files.createTempFile("satviz-lib", null);
     try (InputStream is = NativeLibraryLoader.class.getResourceAsStream(path)) {
