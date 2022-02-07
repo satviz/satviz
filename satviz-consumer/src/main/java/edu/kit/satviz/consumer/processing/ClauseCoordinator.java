0e989f559a0614cae9bc59ef826d29a6dc06a9bd
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-07 14:42:22 +0100

    Begin ClauseCoordinator implementation

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/ClauseCoordinator.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/ClauseCoordinator.java
@@ -3,26 +3,46 @@ package edu.kit.satviz.consumer.processing;
 import edu.kit.satviz.consumer.graph.Graph;
 import edu.kit.satviz.sat.ClauseUpdate;
 
-import java.io.File;
+import edu.kit.satviz.serial.ClauseSerialBuilder;
+import edu.kit.satviz.serial.ClauseSerializer;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeMap;
 
 public class ClauseCoordinator {
 
-  private Path tempDir;
-  private TreeMap<Long, Path> snapshots;
-  private File clauseFile;
+  private final Path tempDir;
+  private final TreeMap<Long, Path> snapshots;
+  private final List<ClauseUpdateProcessor> processors;
+  private final Graph graph;
+  private final RandomAccessFile clauseFile;
+  private final OutputStream clauseStream;
+
+  private long updateCounter;
+  private long currentUpdate;
 
   public ClauseCoordinator(Graph graph, Path tempDir) {
     this.tempDir = tempDir;
+    this.snapshots = new TreeMap<>();
+    this.processors = new ArrayList<>();
+    this.updateCounter = 0;
+    this.graph = graph;
+    this.clauseFile = null;
+    this.clauseStream = null;
   }
 
   public void addProcessor(ClauseUpdateProcessor processor) {
-
+    processors.add(processor);
   }
 
   public void advanceVisualization(int numUpdates) {
-
+    ClauseUpdate[] updates = null; // TODO: get correct updates from file
+    for (ClauseUpdateProcessor processor : processors) {
+      processor.process(updates, graph);
+    }
   }
 
   public long currentUpdate() {
@@ -42,7 +62,8 @@ public class ClauseCoordinator {
   }
 
   public void addClauseUpdate(ClauseUpdate clauseUpdate) {
-
+    updateCounter++;
+    // TODO: Save Clause in clauseFile
   }
 
   public void registerChangeListener(Runnable action) {
