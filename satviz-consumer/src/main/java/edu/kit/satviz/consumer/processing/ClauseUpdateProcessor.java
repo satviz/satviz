96b441b6c5ecc35817c4d496d4c3c5b6fc8996ba
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-07 17:39:24 +0100

    Revert "Use serial module for ClauseUpdateProcessor serialization"
    This reverts commit a2473fb4cee5b712277a44fb29bf2a05d1aca551.

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/ClauseUpdateProcessor.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/ClauseUpdateProcessor.java
@@ -3,13 +3,16 @@ package edu.kit.satviz.consumer.processing;
 import edu.kit.satviz.consumer.graph.Graph;
 import edu.kit.satviz.consumer.graph.GraphUpdate;
 import edu.kit.satviz.sat.ClauseUpdate;
-import edu.kit.satviz.serial.Serializer;
+
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public interface ClauseUpdateProcessor {
 
   GraphUpdate process(ClauseUpdate[] clauseUpdates, Graph graph);
 
-  Serializer<? extends ClauseUpdateProcessor> serializer();
+  void serialize(OutputStream out);
 
+  void deserialize(InputStream in);
 
 }
