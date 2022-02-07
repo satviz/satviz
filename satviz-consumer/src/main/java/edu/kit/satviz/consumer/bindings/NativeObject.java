6c70f10f4362624ea0966876c89c9c56d93626ca
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-07 11:05:20 +0100

    Document NativeObject

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/bindings/NativeObject.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/bindings/NativeObject.java
@@ -11,13 +11,17 @@ import jdk.incubator.foreign.MemoryAddress;
 import jdk.incubator.foreign.MemoryLayout;
 import jdk.incubator.foreign.SymbolLookup;
 
+/**
+ * A class whose instances are coupled to some native memory, such as a C++ object or struct.
+ */
 public abstract class NativeObject implements AutoCloseable {
 
   private static final String PREFIX = "satviz_";
 
   static {
     try {
-      NativeLibraryLoader.loadLibrary("/satviz-consumer-native.so");
+      // to add windows support, select the correct shared library here
+      NativeLibraryLoader.loadLibrary("/libsatviz-consumer-native.so");
     } catch (IOException e) {
       throw new InitializationError("Could not load required native library", e);
     }
@@ -25,10 +29,24 @@ public abstract class NativeObject implements AutoCloseable {
 
   private final MemoryAddress pointer;
 
+  /**
+   * Initializes this {@code NativeObject} with the given pointer to native memory.
+   *
+   * @param pointer The underlying memory address of this object's native component.
+   */
   protected NativeObject(MemoryAddress pointer) {
     this.pointer = pointer;
   }
 
+  /**
+   * Looks up a satviz C function.
+   *
+   * @param name The name of the function, without the "satviz_" prefix.
+   * @param methodType the corresponding Java method signature
+   * @param descriptor the C function signature
+   * @return a {@code MethodHandle} to the looked up function.
+   * @throws UnsatisfiedLinkError if no function matching the given description could be found.
+   */
   public static MethodHandle lookupFunction(
       String name, MethodType methodType, FunctionDescriptor descriptor
   ) {
@@ -38,6 +56,13 @@ public abstract class NativeObject implements AutoCloseable {
     return CLinker.getInstance().downcallHandle(address, methodType, descriptor);
   }
 
+  /**
+   * Creates a {@code structLayout} with the given fields, adding padding where necessary
+   * (as per the C standard).
+   *
+   * @param fields The field layouts of the struct.
+   * @return A {@code MemoryLayout} for a struct.
+   */
   // courtesy of my friend Joshua:
   // https://github.com/IGJoshua/coffi/blob/master/src/clj/coffi/layout.clj
   public static MemoryLayout paddedStruct(MemoryLayout... fields) {
@@ -61,6 +86,11 @@ public abstract class NativeObject implements AutoCloseable {
     return MemoryLayout.structLayout(alignedFields.toArray(new MemoryLayout[0]));
   }
 
+  /**
+   * Returns the underlying pointer to the native component of this object.
+   *
+   * @return a {@code MemoryAddress} to native memory.
+   */
   public MemoryAddress getPointer() {
     return pointer;
   }
