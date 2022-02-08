17f632b57d4bccb9fb45eaea79e37917bdd51de6
Kevin Kuryshev <kuryshevk@gmail.com>
2022-02-08 15:04:25 +0100

    Created test classes for ClauseCoordinator and ExternalClauseBuffer

---
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/ClauseCoordinatorTest.java
@@ -0,0 +1,36 @@
+package edu.kit.satviz.consumer.processing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClauseCoordinatorTest {
+
+  @Test
+  void addProcessor() {
+  }
+
+  @Test
+  void advanceVisualization() {
+  }
+
+  @Test
+  void currentUpdate() {
+  }
+
+  @Test
+  void seekToUpdate() {
+  }
+
+  @Test
+  void takeSnapshot() {
+  }
+
+  @Test
+  void addClauseUpdate() {
+  }
+
+  @Test
+  void registerChangeListener() {
+  }
+}
---
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/ExternalClauseBufferTest.java
@@ -0,0 +1,16 @@
+package edu.kit.satviz.consumer.processing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExternalClauseBufferTest {
+
+  @Test
+  void addClauseUpdate() {
+  }
+
+  @Test
+  void getClauseUpdates() {
+  }
+}
