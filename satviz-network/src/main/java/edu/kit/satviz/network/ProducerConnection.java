019c7fc4707cd3180aa9e97466cabba8d3cc08db
LuWae <l.waeldele@t-online.de>
2022-02-08 21:46:00 +0100

    started ProducerConnection

--- satviz-network/src/main/java/edu/kit/satviz/network/ProducerConnection.java
+++ satviz-network/src/main/java/edu/kit/satviz/network/ProducerConnection.java
@@ -4,16 +4,25 @@ import edu.kit.satviz.sat.ClauseUpdate;
 import edu.kit.satviz.sat.SatAssignment;
 import java.util.function.Consumer;
 
+/**
+ * A clause producer connection to a clause consumer.
+ */
 public class ProducerConnection {
 
   private final ClientConnectionManager conman;
+  private ConnectionId cid = null;
+
+  private boolean done = false;
 
   public ProducerConnection(String address, int port) {
     this.conman = new ClientConnectionManager(address, port, MessageTypes.satvizBlueprint);
   }
 
   public void establish(ProducerId pid) {
-    // TODO
+    conman.registerConnect(this::connectListener);
+    conman.registerGlobalFail(this::globalFailListener);
+    conman.start();
+
   }
 
   public void sendClauseUpdate(ClauseUpdate c) {
@@ -38,5 +47,36 @@ public class ProducerConnection {
 
   public void registerGlobalFail(Consumer<String> ls) {
     // TODO
+  }
+
+
+
+
+  private void connectListener(ConnectionId cid) {
+    this.cid = cid;
+    conman.register(cid, )
+  }
+
+  private void globalFailListener(String reason) {
+    // TODO
+  }
+
+  private void messageListener(ConnectionId cid, NetworkMessage msg) {
+    // ignore cid because there is only one
+    switch (msg.getState()) {
+      case PRESENT -> {
+        switch (msg.getType()) {
+          case MessageTypes.START -> {/*TODO*/}
+          case MessageTypes.STOP -> {/*TODO*/}
+          default -> {/*TODO*/}
+        }
+      }
+      case TERM -> {
+        /*TODO*/
+      }
+      case FAIL -> {
+        /*TODO*/
+      }
+    }
   }
 }
