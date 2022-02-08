f128111439bc3d7a76d497705e47324cf20b80f5
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-08 22:36:34 +0100

    Implement ClauseUpdate serialisation

--- satviz-serial/src/main/java/edu/kit/satviz/serial/ClauseUpdateSerialBuilder.java
+++ satviz-serial/src/main/java/edu/kit/satviz/serial/ClauseUpdateSerialBuilder.java
@@ -1,20 +1,34 @@
 package edu.kit.satviz.serial;
 
+import edu.kit.satviz.sat.Clause;
 import edu.kit.satviz.sat.ClauseUpdate;
 
 public class ClauseUpdateSerialBuilder extends SerialBuilder<ClauseUpdate> {
+
+  // small optimisation because values() has horrible performance characteristics
+  private static final ClauseUpdate.Type[] TYPES = ClauseUpdate.Type.values();
+
+  private final SerialBuilder<Clause> clauseSerialBuilder = new ClauseSerialBuilder();
+
+  private ClauseUpdate.Type type;
+
   @Override
   protected void processAddByte(byte b) throws SerializationException {
-
+    if (type == null) {
+      type = TYPES[b];
+    } else {
+      clauseSerialBuilder.processAddByte(b);
+    }
   }
 
   @Override
   protected ClauseUpdate processGetObject() {
-    return null;
+    return new ClauseUpdate(clauseSerialBuilder.getObject(), type);
   }
 
   @Override
   protected void processReset() {
-
+    type = null;
+    clauseSerialBuilder.reset();
   }
 }
--- satviz-serial/src/main/java/edu/kit/satviz/serial/ClauseUpdateSerializer.java
+++ satviz-serial/src/main/java/edu/kit/satviz/serial/ClauseUpdateSerializer.java
@@ -5,14 +5,18 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class ClauseUpdateSerializer extends Serializer<ClauseUpdate> {
+
+  private static final ClauseSerializer clauseSerializer = new ClauseSerializer();
+
   @Override
   public void serialize(ClauseUpdate clauseUpdate, OutputStream out)
       throws IOException, SerializationException {
-
+    out.write(clauseUpdate.type().ordinal());
+    clauseSerializer.serialize(clauseUpdate.clause(), out);
   }
 
   @Override
   public SerialBuilder<ClauseUpdate> getBuilder() {
-    return null;
+    return new ClauseUpdateSerialBuilder();
   }
 }
