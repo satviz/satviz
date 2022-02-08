cdfbad4cbee5efd33d8c553fd158da101742946d
LuWae <l.waeldele@t-online.de>
2022-02-08 21:11:28 +0100

    ProducerId created

--- satviz-network/src/main/java/edu/kit/satviz/network/ProducerId.java
+++ satviz-network/src/main/java/edu/kit/satviz/network/ProducerId.java
@@ -2,30 +2,9 @@ package edu.kit.satviz.network;
 
 import java.net.InetSocketAddress;
 
-public class ProducerId {
-
-  public ProducerId(InetSocketAddress addr, OfferType type, String solverName,
-      boolean solverDelayed, int instanceHash) {
-    // TODO
-  }
-
-  public InetSocketAddress getAddress() {
-    return null;
-  }
-
-  public OfferType getOfferType() {
-    return null;
-  }
-
-  public String getSolverName() {
-    return null;
-  }
-
-  public boolean getSolverDelayed() {
-    return false;
-  }
-
-  public int getInstanceHash() {
-    return 0;
-  }
+/**
+ * An identifier for a producer connection, with associated data.
+ */
+public record ProducerId(InetSocketAddress addr, OfferType type,
+                         String solverName, boolean solverDelayed, int instanceHash) {
 }
