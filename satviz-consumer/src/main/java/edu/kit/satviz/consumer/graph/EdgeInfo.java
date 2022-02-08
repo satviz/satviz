61426e6853c0ed7cab38c11147c2c3ef069cef41
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-08 16:42:13 +0100

    Use Edge record in EdgeInfo class

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/graph/EdgeInfo.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/graph/EdgeInfo.java
@@ -5,23 +5,17 @@ import jdk.incubator.foreign.MemoryAddress;
 
 public class EdgeInfo extends NativeObject {
 
-  private final int index1;
-  private final int index2;
+  private final Edge edge;
   private final float weight;
 
-  public EdgeInfo(int index1, int index2, float weight) {
+  public EdgeInfo(Edge edge, float weight) {
     super(MemoryAddress.NULL);
-    this.index1 = index1;
-    this.index2 = index2;
+    this.edge = edge;
     this.weight = weight;
   }
 
-  public int getIndex1() {
-    return index1;
-  }
-
-  public int getIndex2() {
-    return index2;
+  public Edge getEdge() {
+    return edge;
   }
 
   public float getWeight() {
