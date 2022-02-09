a59f3865fec06d0b71c0426caa8b20cef3646612
Tim-17 <tim@holzenkamp.eu>
2022-02-09 11:35:52 +0100

    add attributes & fx:id tags for fxml components

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/visualization/VisualizationController.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/gui/visualization/VisualizationController.java
@@ -1,6 +1,43 @@
 package edu.kit.satviz.consumer.gui.visualization;
 
+import edu.kit.satviz.consumer.config.WeightFactor;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
+
 public class VisualizationController {
+
+  @FXML
+  private ChoiceBox<WeightFactor> weightFactorChoiceBox;
+  @FXML
+  private Spinner<Integer> windowSizeSpinner;
+  @FXML
+  private ColorPicker coldColorColorPicker;
+  @FXML
+  private ColorPicker hotColorColorPicker;
+  @FXML
+  private Spinner<Integer> highlightVariableSpinner;
+  @FXML
+  private Button clearHighlightVariableButton;
+  @FXML
+  private Button screenshotButton;
+  @FXML
+  private Button openScreenshotFolderButton;
+  @FXML
+  private Button startOrStopRecordingButton;
+  @FXML
+  private Button pauseOrContinueRecordingButton;
+  @FXML
+  private Button pauseOrContinueVisualizationButton;
+  @FXML
+  private Spinner<Integer> receivedClausesSpinner;
+  @FXML
+  private Slider receivedClausesSlider;
+  @FXML
+  private Button relayoutButton;
 
   // TODO: add constructor with Mediator parameter
 
