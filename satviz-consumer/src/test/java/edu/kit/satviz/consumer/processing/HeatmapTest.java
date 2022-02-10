3bb502037691c45eb67ec6816d265f7a081aecbe
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-10 23:41:00 +0100

    Create test class

---
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/HeatmapTest.java
@@ -0,0 +1,15 @@
+package edu.kit.satviz.consumer.processing;
+
+import edu.kit.satviz.sat.Clause;
+
+public class HeatmapTest {
+
+  private static final Clause[] CLAUSES = {
+      new Clause(new int[] {1, 6, -5, 3}),
+      new Clause(new int[] {5, -4, 2}),
+      new Clause(new int[] {1, -2, 3}),
+      new Clause(new int[] {7, 4, -1}),
+      new Clause(new int[] {-6, 2})
+  };
+
+}
