90a2eac88c3ef7199e042212b4e8955ccbef3360
Kevin Kuryshev <kuryshevk@gmail.com>
2022-02-10 23:29:34 +0100

    Finished the test_advanceVisualization_valid method

--- satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/ClauseCoordinatorTest.java
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/ClauseCoordinatorTest.java
@@ -1,7 +1,6 @@
 package edu.kit.satviz.consumer.processing;
 
 import edu.kit.satviz.consumer.graph.Graph;
-import edu.kit.satviz.sat.Clause;
 import edu.kit.satviz.sat.ClauseUpdate;
 import edu.kit.satviz.serial.SerializationException;
 import java.nio.file.Files;
@@ -9,82 +8,84 @@ import java.nio.file.Paths;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.notNull;
+import static org.mockito.Mockito.*;
 
 class ClauseCoordinatorTest {
 
-  private static final int VARIABLE_AMOUNT = 10;
+  private static final String TEMP_DIR = "src/test/temp";
 
-  private Path tempDir;
+  private int clausesAdded;
+  private int currentlyAdvanced;
   private ClauseCoordinator coordinator;
 
-  private Graph graph = mock(Graph.class);
-
-  private ClauseUpdateProcessor processor1 = mock(ClauseUpdateProcessor.class);
-
-  private ClauseUpdateProcessor processor2 = mock(ClauseUpdateProcessor.class);
+  private Graph graph;
+  private ClauseUpdateProcessor processor1;
+  private ClauseUpdateProcessor processor2;
 
   private final ClauseUpdate[] clauseUpdates = new ClauseUpdate[]{
-          new ClauseUpdate(new Clause(new int[]{6, 1}), ClauseUpdate.Type.ADD),
-          new ClauseUpdate(new Clause(new int[]{6, 2}), ClauseUpdate.Type.ADD),
-          new ClauseUpdate(new Clause(new int[]{6, 3}), ClauseUpdate.Type.ADD),
-          new ClauseUpdate(new Clause(new int[]{-6, 4}), ClauseUpdate.Type.ADD),
-          new ClauseUpdate(new Clause(new int[]{-6, 5}), ClauseUpdate.Type.ADD),
-          new ClauseUpdate(new Clause(new int[]{1, 4}), ClauseUpdate.Type.REMOVE),
-          new ClauseUpdate(new Clause(new int[]{2, 4}), ClauseUpdate.Type.REMOVE),
-          new ClauseUpdate(new Clause(new int[]{3, 4}), ClauseUpdate.Type.REMOVE),
-          new ClauseUpdate(new Clause(new int[]{1, 5}), ClauseUpdate.Type.REMOVE),
-          new ClauseUpdate(new Clause(new int[]{2, 5}), ClauseUpdate.Type.REMOVE),
-          new ClauseUpdate(new Clause(new int[]{3, 5}), ClauseUpdate.Type.REMOVE),
-          new ClauseUpdate(new Clause(new int[]{6}), ClauseUpdate.Type.ADD),
-          new ClauseUpdate(new Clause(new int[]{}), ClauseUpdate.Type.ADD)
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 6, 1),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 6, 2),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 6, 3),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, -6, 4),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, -6, 5),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 1, 4),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 2, 4),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 3, 4),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 1, 5),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 2, 5),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 3, 5),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD, 6),
+          ClauseUpdate.of(ClauseUpdate.Type.ADD)
   };
 
   @BeforeEach
   void setUp() throws IOException {
-    tempDir = Files.createDirectory(Paths.get("src/test"));
+    clausesAdded = 0;
+    currentlyAdvanced = 0;
+    graph = mock(Graph.class);
+    processor1 = mock(ClauseUpdateProcessor.class);
+    processor2 = mock(ClauseUpdateProcessor.class);
+    Path tempDir = Files.createDirectory(Paths.get(TEMP_DIR));
     coordinator = new ClauseCoordinator(graph, tempDir);
     coordinator.addProcessor(processor1);
-    coordinator.addProcessor(processor2);
   }
 
   @Test
-  void advanceVisualization() throws IOException, SerializationException {
-    for (int i = 0; i < 4; i++) {
-      coordinator.addClauseUpdate(clauseUpdates[i]);
+  void test_advanceVisualization_valid() throws IOException, SerializationException {
+    int processor1AdvanceCalls = 0;
+    int processor2AdvanceCalls = 0;
+    for (int i = 4; i < clauseUpdates.length; i += 4) {
+      addUpdatesAndAdvance(Arrays.copyOfRange(clauseUpdates, i - 4, i));
+      processor1AdvanceCalls++;
     }
-    assertEquals(0, coordinator.currentUpdate());
-    coordinator.advanceVisualization(3);
-    assertEquals(3, coordinator.currentUpdate());
-    for (int i = 4; i < 8; i++) {
-      coordinator.addClauseUpdate(clauseUpdates[i]);
+    verify(processor1, times(processor1AdvanceCalls)).process(notNull(), eq(graph));
+    coordinator.addProcessor(processor2);
+    while (clausesAdded < clauseUpdates.length) {
+      addUpdatesAndAdvance(Arrays.copyOfRange(clauseUpdates, clausesAdded, clausesAdded + 1));
+      processor1AdvanceCalls++;
+      processor2AdvanceCalls++;
     }
-    assertEquals(3, coordinator.currentUpdate());
-    coordinator.advanceVisualization(3);
-    assertEquals(6, coordinator.currentUpdate());
-    for (int i = 8; i < 12; i++) {
-      coordinator.addClauseUpdate(clauseUpdates[i]);
+    verify(processor1, times(processor1AdvanceCalls)).process(notNull(), eq(graph));
+    verify(processor2, times(processor2AdvanceCalls)).process(notNull(), eq(graph));
+  }
+
+  private void addUpdatesAndAdvance(ClauseUpdate[] clauseUpdatesToAdd)
+          throws IOException, SerializationException {
+    for (ClauseUpdate update : clauseUpdatesToAdd) {
+      coordinator.addClauseUpdate(update);
+      clausesAdded++;
     }
-    assertEquals(6, coordinator.currentUpdate());
-    coordinator.advanceVisualization(3);
-    assertEquals(9, coordinator.currentUpdate());
-    for (int i = 12; i < 13; i++) {
-      coordinator.addClauseUpdate(clauseUpdates[i]);
-    }
-    assertEquals(9, coordinator.currentUpdate());
-    coordinator.advanceVisualization(3);
-    assertEquals(12, coordinator.currentUpdate());
-    coordinator.advanceVisualization(3);
-    assertEquals(13, coordinator.currentUpdate());
+    assertEquals(currentlyAdvanced, coordinator.currentUpdate());
+    coordinator.advanceVisualization(clauseUpdatesToAdd.length);
+    currentlyAdvanced += clauseUpdatesToAdd.length;
+    assertEquals(currentlyAdvanced, coordinator.currentUpdate());
   }
 
   @AfterEach
