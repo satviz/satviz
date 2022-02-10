1c774c73353145f96be7d2d13c206a1cdb8d1119
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-10 23:54:03 +0100

    Add first tests

--- satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/HeatmapTest.java
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/HeatmapTest.java
@@ -1,8 +1,16 @@
 package edu.kit.satviz.consumer.processing;
 
-import edu.kit.satviz.sat.Clause;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HeatmapTest {
+import edu.kit.satviz.consumer.graph.HeatUpdate;
+import edu.kit.satviz.sat.Clause;
+import edu.kit.satviz.sat.ClauseUpdate;
+import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HeatmapTest {
 
   private static final Clause[] CLAUSES = {
       new Clause(new int[] {1, 6, -5, 3}),
@@ -11,5 +19,35 @@ public class HeatmapTest {
       new Clause(new int[] {7, 4, -1}),
       new Clause(new int[] {-6, 2})
   };
+
+  private static final ClauseUpdate[] UPDATES = Arrays.stream(CLAUSES)
+      .map(c -> new ClauseUpdate(c, ClauseUpdate.Type.ADD))
+      .toArray(ClauseUpdate[]::new);
+
+  private Heatmap heatmap;
+
+  @BeforeEach
+  void setUp() {
+    heatmap = new Heatmap(3);
+  }
+
+  @Test
+  void test_process_single() {
+    var result = heatmap.process(new ClauseUpdate[] {UPDATES[0]}, null);
+    var expected = new HeatUpdate();
+    expected.add(1, 0.25f);
+    expected.add(6, 0.25f);
+    expected.add(5, 0.25f);
+    expected.add(3, 0.25f);
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void test_reset() {
+    heatmap.process(UPDATES, null);
+    heatmap.reset();
+    test_process_single();
+  }
+
 
 }
