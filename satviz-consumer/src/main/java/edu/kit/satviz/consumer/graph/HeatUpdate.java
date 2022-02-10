0f3a6ff0559b90f3ac050d42cf3777f08d9b99e5
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-10 23:42:47 +0100

    Use float instead of int for heat values

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/graph/HeatUpdate.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/graph/HeatUpdate.java
@@ -2,7 +2,7 @@ package edu.kit.satviz.consumer.graph;
 
 public class HeatUpdate implements GraphUpdate {
 
-  public void add(int index, int heat) {
+  public void add(int index, float heat) {
 
   }
 
