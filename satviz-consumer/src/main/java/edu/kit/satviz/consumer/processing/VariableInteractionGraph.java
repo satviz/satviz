8818e698e444df1d56d189e6909df812fef68e20
Kevin Kuryshev <kuryshevk@gmail.com>
2022-02-12 12:55:01 +0100

    Implemented the VariableInteractionGraph class

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/VariableInteractionGraph.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/VariableInteractionGraph.java
@@ -1,27 +1,70 @@
 package edu.kit.satviz.consumer.processing;
 
+import edu.kit.satviz.consumer.config.WeightFactor;
 import edu.kit.satviz.consumer.graph.Graph;
 import edu.kit.satviz.consumer.graph.WeightUpdate;
 import edu.kit.satviz.sat.ClauseUpdate;
-
+import edu.kit.satviz.serial.SerializationException;
+import edu.kit.satviz.serial.StringSerializer;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class VariableInteractionGraph implements ClauseUpdateProcessor {
 
+  private WeightFactor weightFactor;
+  private StringSerializer serializer;
+
+  public VariableInteractionGraph(WeightFactor weightFactor) {
+    this.weightFactor = weightFactor;
+  }
+
+  public void setWeightFactor(WeightFactor weightFactor) {
+    this.weightFactor = weightFactor;
+  }
+
+  public WeightFactor getWeightFactor() {
+    return weightFactor;
+  }
+
   @Override
   public WeightUpdate process(ClauseUpdate[] clauseUpdates, Graph graph) {
-    return null;
+    WeightUpdate weightUpdate = new WeightUpdate();
+    boolean isAdd;
+    int[] literals;
+    float weight;
+    for (ClauseUpdate clauseUpdate : clauseUpdates) {
+      isAdd = clauseUpdate.type() == ClauseUpdate.Type.ADD;
+      literals = clauseUpdate.clause().literals();
+      for (int i = 0; i < literals.length; i++) {
+        for (int j = i; j < literals.length; j++) {
+          if (literals[i] != literals[j]) {
+            weight = (float) weightFactor.apply(literals.length);
+            weightUpdate.add(i, j, (isAdd) ? weight : -weight);
+          }
+        }
+      }
+    }
+    return weightUpdate;
   }
 
   @Override
   public void serialize(OutputStream out) {
-
+    try {
+      serializer.serialize(weightFactor.name(), out);
+    } catch (IOException | SerializationException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
   public void deserialize(InputStream in) {
-
+    try {
+      String weightFactorName = serializer.deserialize(in);
+      weightFactor = WeightFactor.valueOf(weightFactorName);
+    } catch (IOException | SerializationException e) {
+      e.printStackTrace();
+    }
   }
 
 }
