45e7ccdfa66389cbbf50630897231ae1bf4e50f7
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-13 18:34:53 +0100

    Turn EdgeInfo and NodeInfo into records

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/graph/EdgeInfo.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/graph/EdgeInfo.java
@@ -1,23 +1,22 @@
 package edu.kit.satviz.consumer.graph;
 
 import edu.kit.satviz.consumer.bindings.Struct;
-import java.util.Objects;
 import jdk.incubator.foreign.CLinker;
 import jdk.incubator.foreign.MemorySegment;
 
 /**
  * A class that holds information about an edge in a {@link Graph}.
+ *
+ * @param edge the {@link Edge}.
+ * @param weight the weight associated with the edge.
  */
-public final class EdgeInfo {
+public record EdgeInfo(Edge edge, float weight) {
 
   public static final Struct STRUCT = Struct.builder()
       .field("index1", int.class, CLinker.C_INT)
       .field("index2", int.class, CLinker.C_INT)
       .field("weight", float.class, CLinker.C_FLOAT)
       .build();
-
-  private final Edge edge;
-  private final float weight;
 
   /**
    * Create an {@code EdgeInfo} object from a {@code MemorySegment} containing a {@code EdgeInfo}
@@ -29,57 +28,10 @@ public final class EdgeInfo {
    * @param segment the piece of memory holding the {@code EdgeInfo} struct.
    */
   public EdgeInfo(MemorySegment segment) {
-    int index1 = (int) STRUCT.varHandle("index1").get(segment);
-    int index2 = (int) STRUCT.varHandle("index2").get(segment);
-    this.edge = new Edge(index1, index2);
-    this.weight = (float) STRUCT.varHandle("weight").get(segment);
+    this(
+        new Edge((int) STRUCT.varHandle("index1").get(segment),
+            (int) STRUCT.varHandle("index2").get(segment)),
+        (float) STRUCT.varHandle("weight").get(segment)
+    );
   }
-
-  /**
-   * Create an {@code EdgeInfo} object that is not backed by native memory.
-   *
-   * @param edge the {@link Edge}.
-   * @param weight the weight associated with the edge.
-   */
-  public EdgeInfo(Edge edge, float weight) {
-    this.edge = edge;
-    this.weight = weight;
-  }
-
-  /**
-   * Returns the edge described by this object.
-   *
-   * @return an {@link Edge} record.
-   */
-  public Edge getEdge() {
-    return edge;
-  }
-
-  /**
-   * Returns the weight of this edge.
-   *
-   * @return The weight.
-   */
-  public float getWeight() {
-    return weight;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    EdgeInfo edgeInfo = (EdgeInfo) o;
-    return Objects.equals(edgeInfo.edge, edge)
-        && Float.compare(edgeInfo.weight, weight) == 0;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(edge, weight);
-  }
-
 }
--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/graph/NodeInfo.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/graph/NodeInfo.java
@@ -1,14 +1,18 @@
 package edu.kit.satviz.consumer.graph;
 
 import edu.kit.satviz.consumer.bindings.Struct;
-import java.util.Objects;
 import jdk.incubator.foreign.CLinker;
 import jdk.incubator.foreign.MemorySegment;
 
 /**
  * A class that holds information about a node in a {@link Graph}.
+ *
+ * @param index The index of the node described.
+ * @param heat The heat value of the node.
+ * @param x The node's x position
+ * @param y The node's y position
  */
-public final class NodeInfo {
+public record NodeInfo(int index, int heat, float x, float y) {
 
   public static final Struct STRUCT = Struct.builder()
       .field("index", int.class, CLinker.C_INT)
@@ -16,11 +20,6 @@ public final class NodeInfo {
       .field("x", float.class, CLinker.C_FLOAT)
       .field("y", float.class, CLinker.C_FLOAT)
       .build();
-
-  private final int index;
-  private final int heat;
-  private final float x;
-  private final float y;
 
   /**
    * Create a {@code NodeInfo} object from a {@code MemorySegment} containing a {@code NodeInfo}
@@ -32,81 +31,11 @@ public final class NodeInfo {
    * @param segment the piece of memory holding the {@code NodeInfo} struct.
    */
   public NodeInfo(MemorySegment segment) {
-    this.index = (int) STRUCT.varHandle("index").get(segment);
-    this.heat = (int) STRUCT.varHandle("heat").get(segment);
-    this.x = (float) STRUCT.varHandle("x").get(segment);
-    this.y = (float) STRUCT.varHandle("y").get(segment);
+    this(
+        (int) STRUCT.varHandle("index").get(segment),
+        (int) STRUCT.varHandle("heat").get(segment),
+        (float) STRUCT.varHandle("x").get(segment),
+        (float) STRUCT.varHandle("y").get(segment)
+    );
   }
-
-  /**
-   * Create a {@code NodeInfo} object that is not backed by native memory.
-   *
-   * @param index The index of the node described.
-   * @param heat The heat value of the node.
-   * @param x The node's x position
-   * @param y The node's y position
-   */
-  public NodeInfo(int index, int heat, float x, float y) {
-    this.index = index;
-    this.heat = heat;
-    this.x = x;
-    this.y = y;
-  }
-
-  /**
-   * Returns the index of the node.
-   *
-   * @return the index
-   */
-  public int getIndex() {
-    return index;
-  }
-
-  /**
-   * Returns the heat value of the node.
-   *
-   * @return a number from 0-255.
-   */
-  public int getHeat() {
-    return heat;
-  }
-
-  /**
-   * Returns the current x position of the node.
-   *
-   * @return the x position
-   */
-  public float getX() {
-    return x;
-  }
-
-  /**
-   * Returns the current y position of the node.
-   *
-   * @return the y position
-   */
-  public float getY() {
-    return y;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    NodeInfo nodeInfo = (NodeInfo) o;
-    return index == nodeInfo.index
-        && heat == nodeInfo.heat
-        && Float.compare(nodeInfo.x, x) == 0
-        && Float.compare(nodeInfo.y, y) == 0;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(index, heat, x, y);
-  }
-
 }
