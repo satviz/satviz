5330a06a4a23730c675d3d160736e373daf40cfe
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-14 20:40:01 +0100

    Add more test cases

--- satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/HeatmapTest.java
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/HeatmapTest.java
@@ -6,7 +6,6 @@ import edu.kit.satviz.consumer.graph.HeatUpdate;
 import edu.kit.satviz.sat.Clause;
 import edu.kit.satviz.sat.ClauseUpdate;
 import java.util.Arrays;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -35,10 +34,51 @@ class HeatmapTest {
   void test_process_single() {
     var result = heatmap.process(new ClauseUpdate[] {UPDATES[0]}, null);
     var expected = new HeatUpdate();
-    expected.add(1, 0.25f);
-    expected.add(6, 0.25f);
+    expected.add(0, 0.25f);
     expected.add(5, 0.25f);
-    expected.add(3, 0.25f);
+    expected.add(4, 0.25f);
+    expected.add(2, 0.25f);
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void test_process_multiple_overFull() {
+    test_process_multiple_notFull();
+    var result = heatmap.process(Arrays.copyOfRange(UPDATES, 2, 5), null);
+    var expected = new HeatUpdate();
+    expected.add(0, 2f / 3); // 1: 2
+    expected.add(1, 2f / 3); // 2: 2
+    expected.add(2, 1f / 3); // 3: 1
+    expected.add(3, 1f / 3); // 4: 1
+    expected.add(4, 0); // 5: 0
+    expected.add(5, 1f / 3); // 6: 1
+    expected.add(6, 1f / 3); // 7: 1
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void test_process_multiple_notFull() {
+    var result = heatmap.process(Arrays.copyOfRange(UPDATES, 0, 2), null);
+    var expected = new HeatUpdate();
+    expected.add(0, 1f / 2);
+    expected.add(1, 1f / 2);
+    expected.add(2, 1f / 2);
+    expected.add(3, 1f / 2);
+    expected.add(4, 1);
+    expected.add(5, 1f / 2);
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void test_process_multiple_full() {
+    var result = heatmap.process(Arrays.copyOfRange(UPDATES, 0, 3), null);
+    var expected = new HeatUpdate();
+    expected.add(0, 2f / 3); // 0: 2,
+    expected.add(1, 2f / 3); // 1: 2,
+    expected.add(2, 2f / 3); // 2: 2,
+    expected.add(3, 1f / 3); // 3: 1,
+    expected.add(4, 2f / 3); // 4: 2,
+    expected.add(5, 1f / 3); // 5: 1
     assertEquals(expected, result);
   }
 
