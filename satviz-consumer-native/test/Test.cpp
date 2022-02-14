b3f6f88462e34c6808581ab796789b1b1c90f3e5
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2022-02-14 15:28:10 +0100

    Removed dummy unit test.

--- satviz-consumer-native/test/Test.cpp
+++
@@ -1,8 +0,0 @@
-// This file only exists to test out Google Test / CMake / CTest / Github Actions interoperability.
-#include <gtest/gtest.h>
-
-// Dummy Test just to check if testing system works
-TEST(HelloTest, BasicAssertions) {
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
-}
