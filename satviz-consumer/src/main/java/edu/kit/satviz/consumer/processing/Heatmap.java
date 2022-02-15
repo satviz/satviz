c8f2e9d55107d3b3b9e2b1610c93fe365aea8e74
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-15 13:36:50 +0100

    Implement setHeatmapSize properly

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/Heatmap.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/Heatmap.java
@@ -8,22 +8,74 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * An implementation of {@code ClauseUpdateProcessor} that realises a heatmap of variables (nodes).
+ * <br>The heatmap considers the frequency of each variable in the {@code n} most recently
+ * processed clauses and calculates the variable's portion of occurrence based on that.
+ *
+ * @see HeatUpdate
+ */
 public class Heatmap implements ClauseUpdateProcessor {
 
   private final Map<Integer, Integer> frequencies;
   private Clause[] recentClauses;
   private int cursor;
 
+  /**
+   * Create a heatmap with the given initial size.
+   *
+   * @param initialSize The amount of clauses to consider at a time.
+   */
   public Heatmap(int initialSize) {
     this.frequencies = new HashMap<>(initialSize);
     this.recentClauses = new Clause[initialSize];
     this.cursor = 0;
   }
 
+  /**
+   * (Re)set the heatmap size.<br>
+   * This will reset the internal state of
+   *
+   * @param heatmapSize The
+   */
   public void setHeatmapSize(int heatmapSize) {
-    this.frequencies.clear();
-    this.recentClauses = new Clause[heatmapSize];
-    this.cursor = 0;
+    int prevSize = recentClauses.length;
+    if (heatmapSize < prevSize) {
+      shrink(prevSize, heatmapSize);
+    } else if (heatmapSize > prevSize) {
+      grow(prevSize, heatmapSize);
+    }
+  }
+
+  private void shrink(int prevSize, int newSize) {
+    Clause[] temp = new Clause[newSize];
+    if (cursor >= newSize) {
+      int srcPos = cursor - newSize;
+      System.arraycopy(recentClauses, srcPos, temp, 0, newSize);
+      for (int i = 0; i < srcPos; i++) {
+        decreaseFrequencies(recentClauses[i]);
+      }
+      for (int i = cursor; i < prevSize; i++) {
+        decreaseFrequencies(recentClauses[i]);
+      }
+      cursor = 0;
+    } else {
+      System.arraycopy(recentClauses, 0, temp, 0, cursor);
+      int remaining = newSize - cursor;
+      System.arraycopy(recentClauses, prevSize - remaining, temp, cursor, remaining);
+      for (int i = cursor; i < prevSize - remaining; i++) {
+        decreaseFrequencies(recentClauses[i]);
+      }
+    }
+    recentClauses = temp;
+  }
+
+  private void grow(int prevSize, int newSize) {
+    Clause[] temp = new Clause[newSize];
+    System.arraycopy(recentClauses, 0, temp, 0, cursor);
+    int remaining = prevSize - cursor;
+    System.arraycopy(recentClauses, cursor, temp, newSize - remaining, remaining);
+    recentClauses = temp;
   }
 
   public int getHeatmapSize() {
@@ -80,11 +132,13 @@ public class Heatmap implements ClauseUpdateProcessor {
 
   @Override
   public void deserialize(InputStream in) {
-    reset();
+
   }
 
   @Override
   public void reset() {
-    setHeatmapSize(recentClauses.length);
+    this.frequencies.clear();
+    this.recentClauses = new Clause[recentClauses.length];
+    this.cursor = 0;
   }
 }
