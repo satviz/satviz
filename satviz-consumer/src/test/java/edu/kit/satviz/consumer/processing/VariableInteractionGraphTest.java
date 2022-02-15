d211f364ff131b379291c2daa86d3de893ca505d
Kevin Kuryshev <kuryshevk@gmail.com>
2022-02-15 10:13:28 +0100

    Added java-doc to the test class, fixed minor bug and changed the serialization test to be more complex

--- satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/VariableInteractionGraphTest.java
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/processing/VariableInteractionGraphTest.java
@@ -15,17 +15,22 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * The following tests check the functionality of the <code>VariableInteractionGraph</code> class.
+ */
 class VariableInteractionGraphTest {
 
   private static final WeightFactor INITIAL_FACTOR = WeightFactor.CONSTANT;
 
   /**
+   * Clauses, that could be interpreted as an entire DRAT proof:
+   * <pre>
    *         -1 0
    * d -1 -2  3 0
    * d -1 -3 -4 0
    * d -1  2  4 0
    *          2 0
-   *            0
+   *            0</pre>
    */
   private static final ClauseUpdate[] clauseUpdates = new ClauseUpdate[] {
           new ClauseUpdate(new Clause(new int[]{-1}), ClauseUpdate.Type.ADD),
@@ -36,10 +41,13 @@ class VariableInteractionGraphTest {
           new ClauseUpdate(new Clause(new int[]{}), ClauseUpdate.Type.ADD),
   };
 
-  private static final Map<WeightFactor, WeightUpdate> map = new HashMap<>();
+  private static final Map<WeightFactor, WeightUpdate> WEIGHT_UPDATES = new HashMap<>();
 
   private VariableInteractionGraph vig;
 
+  /**
+   * This method initializes the correct weight-updates for them to be checked later on.
+   */
   @BeforeAll
   static void setupBeforeAll() {
     WeightUpdate constantUpdate = new WeightUpdate();
@@ -52,55 +60,69 @@ class VariableInteractionGraphTest {
     constantUpdate.add(-1, 2, 1);
     constantUpdate.add(-1, 4, 1);
     constantUpdate.add(2, 4, 1);
-    map.put(WeightFactor.CONSTANT, constantUpdate);
+    WEIGHT_UPDATES.put(WeightFactor.CONSTANT, constantUpdate);
 
     WeightUpdate reciprocalUpdate = new WeightUpdate();
-    constantUpdate.add(-1, -2, -1f / 3f);
-    constantUpdate.add(-1, 3, -1f / 3f);
-    constantUpdate.add(-2, 3, -1f / 3f);
-    constantUpdate.add(-1, -3, -1f / 3f);
-    constantUpdate.add(-1, -4, -1f / 3f);
-    constantUpdate.add(-3, -4, -1f / 3f);
-    constantUpdate.add(-1, 2, 1f / 3f);
-    constantUpdate.add(-1, 4, 1f / 3f);
-    constantUpdate.add(2, 4, 1f / 3f);
-    map.put(WeightFactor.RECIPROCAL, reciprocalUpdate);
+    reciprocalUpdate.add(-1, -2, -1f / 3f);
+    reciprocalUpdate.add(-1, 3, -1f / 3f);
+    reciprocalUpdate.add(-2, 3, -1f / 3f);
+    reciprocalUpdate.add(-1, -3, -1f / 3f);
+    reciprocalUpdate.add(-1, -4, -1f / 3f);
+    reciprocalUpdate.add(-3, -4, -1f / 3f);
+    reciprocalUpdate.add(-1, 2, 1f / 3f);
+    reciprocalUpdate.add(-1, 4, 1f / 3f);
+    reciprocalUpdate.add(2, 4, 1f / 3f);
+    WEIGHT_UPDATES.put(WeightFactor.RECIPROCAL, reciprocalUpdate);
 
     WeightUpdate exponentialUpdate = new WeightUpdate();
-    constantUpdate.add(-1, -2, -1f / (float) Math.pow(2, 3));
-    constantUpdate.add(-1, 3, -1f / (float) Math.pow(2, 3));
-    constantUpdate.add(-2, 3, -1f / (float) Math.pow(2, 3));
-    constantUpdate.add(-1, -3, -1f / (float) Math.pow(2, 3));
-    constantUpdate.add(-1, -4, -1f / (float) Math.pow(2, 3));
-    constantUpdate.add(-3, -4, -1f / (float) Math.pow(2, 3));
-    constantUpdate.add(-1, 2, 1f / (float) Math.pow(2, 3));
-    constantUpdate.add(-1, 4, 1f / (float) Math.pow(2, 3));
-    constantUpdate.add(2, 4, 1f / (float) Math.pow(2, 3));
-    map.put(WeightFactor.EXPONENTIAL, exponentialUpdate);
+    exponentialUpdate.add(-1, -2, -1f / (float) Math.pow(2, 3));
+    exponentialUpdate.add(-1, 3, -1f / (float) Math.pow(2, 3));
+    exponentialUpdate.add(-2, 3, -1f / (float) Math.pow(2, 3));
+    exponentialUpdate.add(-1, -3, -1f / (float) Math.pow(2, 3));
+    exponentialUpdate.add(-1, -4, -1f / (float) Math.pow(2, 3));
+    exponentialUpdate.add(-3, -4, -1f / (float) Math.pow(2, 3));
+    exponentialUpdate.add(-1, 2, 1f / (float) Math.pow(2, 3));
+    exponentialUpdate.add(-1, 4, 1f / (float) Math.pow(2, 3));
+    exponentialUpdate.add(2, 4, 1f / (float) Math.pow(2, 3));
+    WEIGHT_UPDATES.put(WeightFactor.EXPONENTIAL, exponentialUpdate);
   }
 
+  /**
+   * This method initializes an instance of the <code>VariableInteractionGraph</code> class.
+   */
   @BeforeEach
   void setupBeforeEach() {
     vig = new VariableInteractionGraph(INITIAL_FACTOR);
   }
 
+  /**
+   * This test processes the clause-updates with each possible weight-factor.
+   */
   @Test
   void test_process_eachWeightFactor() {
     for (WeightFactor factor : WeightFactor.values()) {
       vig.setWeightFactor(factor);
-      assertEquals(map.get(factor), vig.process(clauseUpdates, null));
+      assertEquals(WEIGHT_UPDATES.get(factor), vig.process(clauseUpdates, null));
     }
   }
 
+  /**
+   * This test serializes and deserializes the vig and checks, whether the state stays
+   * consistent after deserialization.
+   */
   @Test
   void test_serializeAndDeserialize_full() {
     for (WeightFactor factor : WeightFactor.values()) {
       vig.setWeightFactor(factor);
       ByteArrayOutputStream out = new ByteArrayOutputStream();
       vig.serialize(out);
-      ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-      vig.deserialize(in);
-      assertEquals(factor, vig.getWeightFactor());
+      ByteArrayInputStream in;
+      for (WeightFactor factor2 : WeightFactor.values()) {
+        in = new ByteArrayInputStream(out.toByteArray());
+        vig.setWeightFactor(factor2);
+        vig.deserialize(in);
+        assertEquals(factor, vig.getWeightFactor());
+      }
     }
   }
 
