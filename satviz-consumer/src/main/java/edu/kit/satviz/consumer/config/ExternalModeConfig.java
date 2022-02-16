648d47fd57268d3b9f195f405a3dc678cc9890e9
Tim-17 <tim@holzenkamp.eu>
2022-02-16 15:43:09 +0100

    use correct default port number

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/config/ExternalModeConfig.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/config/ExternalModeConfig.java
@@ -12,7 +12,7 @@ public class ExternalModeConfig extends ConsumerModeConfig {
 
   public static final int MIN_PORT_NUMBER = 1;
   public static final int MAX_PORT_NUMBER = 65535;
-  public static final int DEFAULT_PORT_NUMBER = 34321;
+  public static final int DEFAULT_PORT_NUMBER = 34312;
 
   private int port;
 
