61dc02c1a2befef0fa53fe772d824709269c0bb4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-17 17:56:30 +0100

    add Hashing util class to common

---
+++ satviz-common/src/main/java/edu/kit/satviz/common/Hashing.java
@@ -0,0 +1,32 @@
+package edu.kit.satviz.common;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import net.jpountz.xxhash.XXHashFactory;
+
+public final class Hashing {
+
+  private static final int HASH_SEED = 34312;
+  private static final XXHashFactory HASH_FACTORY = XXHashFactory.fastestInstance();
+
+  private Hashing() {
+
+  }
+
+  public static long hashFile(Path file) throws IOException {
+    byte[] buf = new byte[8192];
+    try (
+        var hash = HASH_FACTORY.newStreamingHash64(HASH_SEED);
+        var stream = new BufferedInputStream(Files.newInputStream(file), 8192)
+    ) {
+      int read;
+      while ((read = stream.read(buf)) != -1) {
+        hash.update(buf, 0, read);
+      }
+      return hash.getValue();
+    }
+  }
+
+}
