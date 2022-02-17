3305e2f79b8def9a6a66d9612cb53c2bc5ad974e
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-17 18:19:49 +0100

    Add hashing test

--- satviz-common/src/main/java/edu/kit/satviz/common/Hashing.java
+++ satviz-common/src/main/java/edu/kit/satviz/common/Hashing.java
@@ -3,13 +3,10 @@ package edu.kit.satviz.common;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import net.jpountz.xxhash.XXHashFactory;
 
 public final class Hashing {
 
-  private static final int HASH_SEED = 34312;
   private static final XXHashFactory HASH_FACTORY = XXHashFactory.fastestInstance();
 
   private Hashing() {
@@ -19,7 +16,7 @@ public final class Hashing {
   public static long hashContent(InputStream in) throws IOException {
     byte[] buf = new byte[8192];
     try (
-        var hash = HASH_FACTORY.newStreamingHash64(HASH_SEED);
+        var hash = HASH_FACTORY.newStreamingHash64(0);
         var stream = new BufferedInputStream(in, 8192)
     ) {
       int read;
