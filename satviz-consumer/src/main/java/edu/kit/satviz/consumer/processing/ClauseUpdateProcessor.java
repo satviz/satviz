a1b5b719caabfb9302749c4b22f40ce6c7d00e9e
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-19 19:53:17 +0100

    Document ClauseUpdateProcessor

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/ClauseUpdateProcessor.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/ClauseUpdateProcessor.java
@@ -6,18 +6,43 @@ import edu.kit.satviz.sat.ClauseUpdate;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * This interface allows for instances of the <code>ClauseUpdate</code> class to be
+ * processed into <code>GraphUpdate</code>s.
+ */
 public interface ClauseUpdateProcessor {
 
+  /**
+   * This method processes an array of the {@code ClauseUpdate}s into {@code GraphUpdate}s.
+   *
+   * @param clauseUpdates An array of {@code ClauseUpdate}s.
+   * @param graph An instance of the {@code Graph} class.
+   * @return An instance of the {@code GraphUpdate} class.
+   */
   GraphUpdate process(ClauseUpdate[] clauseUpdates, Graph graph);
 
+  /**
+   * This method serializes the internal state of the processor.<br>
+   * <i>Note: Not every processor needs internal state.</i>
+   *
+   * @param out The {@code OutputStream} this processor's state is serialized to.
+   */
   default void serialize(OutputStream out) {
 
   }
 
+  /**
+   * This method deserializes the internal state of the processor from a given {@code InputStream}.
+   *
+   * @param in The {@code InputStream} this processor's state is deserialized from.
+   */
   default void deserialize(InputStream in) {
 
   }
 
+  /**
+   * This method resets this processor to its original state.
+   */
   default void reset() {
 
   }
