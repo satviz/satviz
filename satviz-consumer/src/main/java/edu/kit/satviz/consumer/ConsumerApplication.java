5c616a09dc262fac791b0b7cb02514a225eb07e5
Kevin Kuryshev <kuryshevk@gmail.com>
2022-02-19 13:29:27 +0100

    Implemented the network connection setup in the ConsumerApplication

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/ConsumerApplication.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/ConsumerApplication.java
@@ -1,6 +1,10 @@
 package edu.kit.satviz.consumer;
 
 import edu.kit.satviz.consumer.config.ConsumerConfig;
+import edu.kit.satviz.consumer.config.ConsumerMode;
+import edu.kit.satviz.consumer.config.ExternalModeConfig;
+import edu.kit.satviz.consumer.display.DisplayType;
+import edu.kit.satviz.consumer.display.VideoController;
 import edu.kit.satviz.consumer.graph.Graph;
 import edu.kit.satviz.consumer.gui.config.ConfigStarter;
 import edu.kit.satviz.consumer.gui.visualization.VisualizationController;
@@ -9,30 +13,47 @@ import edu.kit.satviz.consumer.processing.ClauseCoordinator;
 import edu.kit.satviz.consumer.processing.Heatmap;
 import edu.kit.satviz.consumer.processing.Mediator;
 import edu.kit.satviz.consumer.processing.VariableInteractionGraph;
+import edu.kit.satviz.network.ConsumerConnection;
 import edu.kit.satviz.network.ConsumerConnectionListener;
 import edu.kit.satviz.network.ProducerId;
 import edu.kit.satviz.parsers.DimacsFile;
-
+import edu.kit.satviz.parsers.ParsingException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
 
-public class ConsumerApplication {
+public final class ConsumerApplication {
 
-  public static void main(String[] args) throws FileNotFoundException {
+  private static ProducerId pid = null;
+  private static final Object SYNC_OBJECT = new Object();
+
+  public static void main(String[] args) throws FileNotFoundException, InterruptedException {
     ConsumerConfig config = getStartingConfig(args);
     int variableAmount;
-    try (DimacsFile dimacsFile = new DimacsFile(new FileInputStream(config.getInstancePath().toString()))) {
-      variableAmount = dimacsFile.getVariableAmount();
+    try {
+      try (DimacsFile dimacsFile = new DimacsFile(
+          new FileInputStream(config.getInstancePath().toString()))) {
+        variableAmount = dimacsFile.getVariableAmount();
+      }
+    } catch (ParsingException e) {
+      if (!config.isNoGui()) {
+        // Error window.
+      }
+      throw e;
     }
-    int port = 0; // TODO: Either EMBEDDED port or in ConsumerConfig
+
     Graph graph = Graph.create(variableAmount);
-    ClauseCoordinator coordinator = new ClauseCoordinator(graph, null); // TODO: get tempDir!
-    Heatmap heatmap = new Heatmap();
+    VideoController videoController = VideoController.create(
+        graph,
+        (config.isNoGui()) ? DisplayType.OFFSCREEN : DisplayType.ONSCREEN
+    );
+    ClauseCoordinator coordinator = new ClauseCoordinator(graph, createTempDir());
+    Heatmap heatmap = new Heatmap(); //
     VariableInteractionGraph vig = new VariableInteractionGraph();
 
-    Mediator mediator = null; // TODO: Get Mediator!
-    ConsumerConnectionListener connectionListener = setupNetworkConnection(port);
-    ProducerId producerId = waitForFirstProducer(connectionListener);
+    ConsumerConnection connection = setupNetworkConnection(config);
+
+    Mediator mediator = null; // MEDIATOR
 
     if (!config.isNoGui()) {
       VisualizationController vController = new VisualizationController(mediator, config);
@@ -42,7 +63,8 @@ public class ConsumerApplication {
       VisualizationStarter.launch();
     }
 
-    // TODO: Connect to Producer!
+    connection.connect(ConsumerApplication.pid, null); // MEDIATOR
+
     // TODO: Start main-loop
     // TODO: End
     // TODO: Exceptions!
@@ -58,14 +80,46 @@ public class ConsumerApplication {
     }
   }
 
-  private static ConsumerConnectionListener setupNetworkConnection(int port) {
-    // TODO: 16.02.2022
-    return null;
+  private static ConsumerConnection setupNetworkConnection(ConsumerConfig config)
+      throws InterruptedException {
+    int consumerPort;
+    boolean isEmbedded = config.getModeConfig().getMode() == ConsumerMode.EMBEDDED;
+    if (isEmbedded) {
+      consumerPort = 0;
+    } else {
+      consumerPort = ((ExternalModeConfig) config.getModeConfig()).getPort();
+    }
+    ConsumerConnection connection = new ConsumerConnection(consumerPort);
+    connection.registerConnect(ConsumerApplication::newConnectionAvailable);
+    if (isEmbedded) {
+      // TODO: 19.02.2022 START PRODUCER!
+      connection.getPort();
+    }
+    synchronized (SYNC_OBJECT) {
+      while (ConsumerApplication.pid == null) {
+        SYNC_OBJECT.wait();
+      }
+    }
+    return connection;
   }
 
   private static ProducerId waitForFirstProducer(ConsumerConnectionListener connectionListener) {
     // TODO: 16.02.2022
     return null;
+  }
+
+  private static Path createTempDir() {
+    // TODO: 19.02.2022
+    return null;
+  }
+
+  private static void newConnectionAvailable(ProducerId pid) {
+    synchronized (SYNC_OBJECT) {
+      if (ConsumerApplication.pid == null) {
+        ConsumerApplication.pid = pid;
+        SYNC_OBJECT.notifyAll();
+      }
+    }
   }
 
 }
