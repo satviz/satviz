0a47d717033be411a420e91e931ca16da1f3b203
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-20 22:09:04 +0100

    Further comment heatmap

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/Heatmap.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/Heatmap.java
@@ -50,19 +50,26 @@ public class Heatmap implements ClauseUpdateProcessor {
   private void shrink(int prevSize, int newSize) {
     Clause[] temp = new Clause[newSize];
     if (cursor >= newSize) {
+      /* shrinking, case "all remaining elements are between 0 and cursor":
+         [1, 2, ..., s, ..., >c, c+1, ..., n] => [>s, ..., c-1] */
       int srcPos = cursor - newSize;
       System.arraycopy(recentClauses, srcPos, temp, 0, newSize);
+      /* Decrement [1, 2, ..., s-1] */
       for (int i = 0; i < srcPos; i++) {
         decreaseFrequencies(recentClauses[i]);
       }
+      /* Decrement [c, ..., n] */
       for (int i = cursor; i < prevSize; i++) {
         decreaseFrequencies(recentClauses[i]);
       }
       cursor = 0;
     } else {
+      /* shrinking, case "0 to cursor + elements from the end are included":
+         [1, 2, ..., >c, c+1, ..., s, s+1, ..., n] => [1, 2, ..., >s, s+1, ..., n] */
       System.arraycopy(recentClauses, 0, temp, 0, cursor);
       int remaining = newSize - cursor;
       System.arraycopy(recentClauses, prevSize - remaining, temp, cursor, remaining);
+      /* Decrement [c, ..., s-1] */
       for (int i = cursor; i < prevSize - remaining; i++) {
         decreaseFrequencies(recentClauses[i]);
       }
@@ -70,6 +77,8 @@ public class Heatmap implements ClauseUpdateProcessor {
     recentClauses = temp;
   }
 
+  /* Growing the ring buffer:
+     [1, 2, ..., >c, c+1, ..., n] => [1, 2, ..., >c, null, null, ..., c+1, ..., n] */
   private void grow(int prevSize, int newSize) {
     Clause[] temp = new Clause[newSize];
     System.arraycopy(recentClauses, 0, temp, 0, cursor);
@@ -78,6 +87,11 @@ public class Heatmap implements ClauseUpdateProcessor {
     recentClauses = temp;
   }
 
+  /**
+   * Get the heatmap size currently being used.
+   *
+   * @return the heatmap window size.
+   */
   public int getHeatmapSize() {
     return recentClauses.length;
   }
@@ -90,6 +104,8 @@ public class Heatmap implements ClauseUpdateProcessor {
       cursor = (cursor + i) % recentClauses.length;
       Clause previous = recentClauses[cursor];
       if (previous != null) {
+        // if we encounter an existing element, the ring buffer is full and
+        // the variables' frequencies need to be decremented
         full = true;
         decreaseFrequencies(previous);
       }
@@ -101,11 +117,14 @@ public class Heatmap implements ClauseUpdateProcessor {
     return populateUpdate(totalAmount);
   }
 
+  /* Calculate the updated heat values for each node based on its frequency and the total amount
+     of nodes currently being updated. */
   private HeatUpdate populateUpdate(int totalAmount) {
     HeatUpdate update = new HeatUpdate();
     var iterator = frequencies.entrySet().iterator();
     while (iterator.hasNext()) {
       var entry = iterator.next();
+      // key - 1 here to convert between 1-indexed variables and 0-indexed graph nodes
       update.add(entry.getKey() - 1, (float) entry.getValue() / totalAmount);
       if (entry.getValue() == 0) {
         iterator.remove();
@@ -114,12 +133,14 @@ public class Heatmap implements ClauseUpdateProcessor {
     return update;
   }
 
+  /* Increment the frequencies of the variables in a clause */
   private void increaseFrequencies(Clause clause) {
     for (int literal : clause.literals()) {
       frequencies.compute(Math.abs(literal), (k, v) -> v == null ? 1 : v + 1);
     }
   }
 
+  /* Decrement the frequencies of the variables in a clause - not going lower than 0 */
   private void decreaseFrequencies(Clause clause) {
     for (int literal : clause.literals()) {
       int variable = Math.abs(literal);
@@ -130,6 +151,8 @@ public class Heatmap implements ClauseUpdateProcessor {
     }
   }
 
+  /* The heatmap currently doesn't serialise anything, so when a different state is loaded
+     it simply resets and starts from scratch. */
   @Override
   public void deserialize(InputStream in) {
     reset();
