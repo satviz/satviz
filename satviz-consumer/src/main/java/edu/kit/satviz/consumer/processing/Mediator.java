62ad34d5bf80aafde030bdc0e86b1542c6a92be4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-02-25 17:47:09 +0100

    Use frame task queue for consistency

--- satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/Mediator.java
+++ satviz-consumer/src/main/java/edu/kit/satviz/consumer/processing/Mediator.java
@@ -10,6 +10,8 @@ import edu.kit.satviz.sat.ClauseUpdate;
 import edu.kit.satviz.sat.SatAssignment;
 import edu.kit.satviz.serial.SerializationException;
 import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -25,6 +27,7 @@ public class Mediator implements ConsumerConnectionListener {
   private final ConsumerConfig config;
   private final ScheduledExecutorService glScheduler;
   private final long period;
+  private final Queue<Runnable> taskQueue;
 
   private boolean recording;
   private boolean recordingPaused;
@@ -54,6 +57,7 @@ public class Mediator implements ConsumerConnectionListener {
     this.visualizationPaused = true;
     this.clausesPerAdvance = config.getBufferSize();
     this.period = config.getPeriod();
+    this.taskQueue = new LinkedBlockingQueue<>();
     //System.out.println("Period: " + period + ", buffer: " + clausesPerAdvance);
     coordinator.addProcessor(heatmap);
     coordinator.addProcessor(vig);
@@ -64,7 +68,7 @@ public class Mediator implements ConsumerConnectionListener {
   }
 
   public void updateWindowSize(int windowSize) {
-    glScheduler.submit(() -> heatmap.setHeatmapSize(windowSize));
+    taskQueue.offer(() -> heatmap.setHeatmapSize(windowSize));
   }
 
   public void updateHeatmapColdColor(Color color) {
@@ -101,11 +105,14 @@ public class Mediator implements ConsumerConnectionListener {
 
   public void startOrStopRecording() {
     if (recording) {
-      glScheduler.submit(videoController::finishRecording);
+      if (recordingPaused) {
+        taskQueue.offer(videoController::resumeRecording);
+      }
+      taskQueue.offer(videoController::finishRecording);
     } else {
       String filename = config.getVideoTemplatePath()
           .replace("{}", String.valueOf(++recordedVideos));
-      glScheduler.submit(() -> videoController.startRecording(filename, "theora"));
+      taskQueue.offer(() -> videoController.startRecording(filename, "theora"));
     }
     recordingPaused = false;
     recording = !recording;
@@ -114,9 +121,9 @@ public class Mediator implements ConsumerConnectionListener {
   public void pauseOrContinueRecording() {
     if (recording) {
       if (recordingPaused) {
-        glScheduler.submit(videoController::resumeRecording);
+        taskQueue.offer(videoController::resumeRecording);
       } else {
-        glScheduler.submit(videoController::stopRecording);
+        taskQueue.offer(videoController::stopRecording);
       }
       recordingPaused = !recordingPaused;
     }
@@ -137,11 +144,11 @@ public class Mediator implements ConsumerConnectionListener {
   }
 
   public void relayout() {
-    glScheduler.submit(graph::recalculateLayout);
+    taskQueue.offer(graph::recalculateLayout);
   }
 
   public void seekToUpdate(long index) {
-    glScheduler.submit(() -> {
+    taskQueue.offer(() -> {
       try {
         coordinator.seekToUpdate(index);
       } catch (Throwable e) { // TODO: 10/02/2022
@@ -175,6 +182,9 @@ public class Mediator implements ConsumerConnectionListener {
       }
       //System.out.println("Post advance");
       videoController.nextFrame();
+      while (!taskQueue.isEmpty()) {
+        taskQueue.poll().run();
+      }
       //System.out.println("Post nextframe");
     } catch (Throwable e) {
       e.printStackTrace();
