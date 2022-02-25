a931e08425d7e04117cb0063a675eefdbca58a17
Kevin Kuryshev <kuryshevk@gmail.com>
2022-02-25 13:41:19 +0100

    Created empty ConsumerCliTest class

---
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/cli/ConsumerCliTest.java
@@ -0,0 +1,16 @@
+package edu.kit.satviz.consumer.cli;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsumerCliTest {
+
+
+
+  @BeforeAll
+  void setUp() {
+
+  }
+
+}
