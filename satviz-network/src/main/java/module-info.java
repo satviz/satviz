796ccc70e21eac4c2a77c390919d9b20a2c26ad0
LuWae <l.waeldele@t-online.de>
2022-03-05 15:26:13 +0100

    updated module-info to only export pub packet

--- satviz-network/src/main/java/module-info.java
+++ satviz-network/src/main/java/module-info.java
@@ -3,7 +3,5 @@ module edu.kit.satviz.network {
   requires transitive edu.kit.satviz.sat;
   requires edu.kit.satviz.serial;
 
-  exports edu.kit.satviz.network;
-    exports edu.kit.satviz.network.general;
-    exports edu.kit.satviz.network.pub;
+  exports edu.kit.satviz.network.pub;
 }
