a6d56389cfb942b1b6da238a56773541d13233a8
LuWae <l.waeldele@t-online.de>
2022-03-11 09:38:20 +0100

    filled in most of ProducerConnection

--- satviz-network/src/main/java/edu/kit/satviz/network/pub/ProducerConnection.java
+++ satviz-network/src/main/java/edu/kit/satviz/network/pub/ProducerConnection.java
@@ -1,21 +1,27 @@
 package edu.kit.satviz.network.pub;
 
 import edu.kit.satviz.network.general.Connection;
+import edu.kit.satviz.network.general.NetworkMessage;
 import edu.kit.satviz.sat.ClauseUpdate;
+import edu.kit.satviz.sat.SatAssignment;
 import edu.kit.satviz.serial.SerializationException;
 
 import java.io.IOException;
 import java.net.ConnectException;
+import java.nio.channels.ClosedChannelException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Queue;
 
 public class ProducerConnection {
   private enum State {
     INIT,
-    CONNECTING,
-    CONNECTED,
-    STOPPED
+    ESTABLISHING,
+    ESTABLISHED,
+    STARTED,
+    CLOSING,
+    CLOSED
   }
 
   private final String address;
@@ -24,32 +30,35 @@ public class ProducerConnection {
   private final ProducerId pid;
   private final ProducerConnectionListener ls;
 
-  private final Object SYNC_READ = new Object();
   private final Object SYNC_STATE = new Object();
-  private volatile boolean shouldClose = false;
+  private volatile State state = State.INIT;
 
   public ProducerConnection(String address, int port, ProducerId pid, ProducerConnectionListener ls) {
     this.address = address;
     this.port = port;
-    this.pid = Objects.requireNonNull(pid);
+    this.pid = pid;
     this.ls = Objects.requireNonNull(ls);
   }
 
-  private void doEstablish() throws IOException, InterruptedException {
-    synchronized (SYNC_STATE) { // TODO no explicit sync here; boolean flag; but take care of close
-      do {
-        try {
-          client = new Connection(address, port, MessageTypes.satvizBlueprint);
-        } catch (ConnectException e) {
-          // connection refused by remote machine (no-one listening on port)
-          // try again later
-          client = null;
-          // we do not expect to be woken up here, but spurious wake-ups don't matter
-          SYNC_STATE.wait(1000);
-        }
-      } while (client == null && !shouldClose);
-    }
+  private void doClose() {
+    synchronized (SYNC_STATE) {
+      if (state == State.CLOSED) {
+        return;
+      }
+      state = State.CLOSED;
 
+      try {
+        client.write(MessageTypes.TERM_FAIL, "internal failure");
+      } catch (Exception e) {
+        // ignore because there is nothing we can do now
+      }
+      if (client != null) {
+        client.close();
+      }
+    }
+  }
+
+  private boolean doEstablish() {
     Map<String, String> offerData = new HashMap<>();
     offerData.put("version", "1");
     if (pid.type() == OfferType.SOLVER) {
@@ -61,30 +70,178 @@ public class ProducerConnection {
     } else {
       offerData.put("type", "proof");
     }
-    try {
-      client.write(MessageTypes.OFFER, offerData);
-    } catch (SerializationException e) {
-      throw new RuntimeException(e); // TODO
+
+    synchronized (SYNC_STATE) {
+      while (client == null && state == State.ESTABLISHING) {
+        try {
+          client = new Connection(address, port, MessageTypes.satvizBlueprint);
+        } catch (ConnectException e) {
+          // connection refused by remote machine (no-one listening on port)
+          // try again later
+          client = null;
+          // we do not expect to be woken up here, but spurious wake-ups don't matter
+          try {
+            SYNC_STATE.wait(1000);
+          } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+            doClose();
+            return false;
+          }
+        } catch (Exception e) {
+          doClose();
+          return false;
+        }
+      }
+
+      if (client == null) { // got signal that we should close
+        doClose();
+        return false;
+      }
+
+      try {
+        client.write(MessageTypes.OFFER, offerData);
+      } catch (Exception e) {
+        doClose();
+        return false;
+      }
+
+      state = State.ESTABLISHED;
+      return true;
     }
   }
 
-  public void sendClauseUpdate(ClauseUpdate c) throws IOException, SerializationException {
-    synchronized (SYNC_READ) {
+  private void threadMain() {
+    if (!doEstablish()) {
+      // state is set to closed and connection is closed
+      return;
+    }
+
+    // Note: we cannot have the read() call in a synchronized block, as this would interfere
+    // with writing. Waiting for up to one second in read() while we cannot write is not desirable.
+    // This means we have to be careful here, as we may read past a close(). Sometimes this is
+    // unavoidable, and not indicative of a synchronization mistake.
+
+    Queue<NetworkMessage> readQueue;
+    while (true) {
       synchronized (SYNC_STATE) {
-        if (...) {
-          if (c.type() == ClauseUpdate.Type.ADD) {
-            client.write(MessageTypes.CLAUSE_ADD, c.clause());
-          } else {
-            client.write(MessageTypes.CLAUSE_DEL, c.clause());
+        if (state == State.CLOSING) {
+          doClose();
+          return;
+        }
+      }
+
+      try {
+        readQueue = client.read();
+      } catch (ClosedChannelException e) { // includes AsynchronousCloseException
+        // someone called close; we don't need to do that here
+        return;
+      } catch (IOException | SerializationException e) {
+        doClose();
+        return;
+      }
+
+      for (NetworkMessage msg: readQueue) {
+        switch (msg.getType()) {
+          case MessageTypes.START -> {
+            synchronized (SYNC_STATE) {
+              if (state == State.CLOSING || state == State.CLOSED) {
+                doClose();
+                return;
+              }
+              state = State.STARTED;
+            }
           }
+          case MessageTypes.STOP -> {
+            doClose();
+            return;
+          }
+          default -> { /* ignore */ }
         }
       }
     }
   }
 
-  public void close() {
+  /**
+   * @throws IllegalStateException if {@code establish()} has already been called or this
+   *     ProducerConnection is closed
+   */
+  public void establish() {
     synchronized (SYNC_STATE) {
-      shouldClose = true;
+      if (state != State.INIT) {
+        throw new IllegalStateException("establish already called or connection closed");
+      }
+      state = State.ESTABLISHING;
+      new Thread(this::threadMain).start();
+    }
+  }
+
+  public void sendClauseUpdate(ClauseUpdate c) throws SerializationException {
+    // Note: I thought about using something like a ReadWriteLock here to allow more than one
+    // concurrent clause writer, but I decided against it since all writes happen in serial order
+    // in the Connection anyway.
+    // We still need to synchronize here to make sure that no clause updates are sent after a
+    // termination message. Most likely the monitor is free, as the only other synchronization-
+    // expensive operation (establishing) has finished already.
+    byte type = c.type() == ClauseUpdate.Type.ADD ?
+        MessageTypes.CLAUSE_ADD : MessageTypes.CLAUSE_DEL;
+
+    synchronized (SYNC_STATE) {
+      if (state != State.STARTED) {
+        // do something. Don't throw an exception, because we may not be able to avoid
+        // clauses being written after close() is called. We usually just want to ignore them.
+        // but the user needs some kind of signal if something went wrong
+        // TODO isn't this exactly what the onDisconnect is for?
+        // TODO make sure that onDisconnect is not ignored, if you use it (2nd call to doClose)
+      }
+      try {
+        client.write(type, c.clause());
+      } catch (IOException e) {
+        doClose();
+      }
+      // note: socket is not closed on a SerializationException
+    }
+  }
+
+  public void terminateSolved(SatAssignment assign) {
+    synchronized (SYNC_STATE) {
+      if (state == State.INIT || state == State.ESTABLISHING || state == State.ESTABLISHED) {
+        throw new IllegalStateException("terminate before connection is established and started");
+      }
+      if (state == State.STARTED) {
+        try {
+          client.write(MessageTypes.TERM_SOLVE, assign);
+        } catch (Exception e) {
+          // nothing more we can do
+        }
+        doClose(); // TODO remove send there
+      }
+      // otherwise we do nothing
+      // TODO don't we want the reader thread to handle termination?
+      // TODO if so, do we even want direct doClose() calls in any of these methods?
+
+      // TODO are there different conventions for terminateFailed and the others?
+    }
+  }
+
+  public void terminateRefuted() {
+    synchronized (SYNC_STATE) {
+      // TODO
+    }
+  }
+
+  public void terminateFailed(String reason) {
+    synchronized (SYNC_STATE) {
+      // TODO
+    }
+  }
+
+  public void close() {
+    // TODO
+    synchronized (SYNC_STATE) {
+      if (state == State.CLOSING || state == State.CLOSED) {
+        return;
+      }
+      state = State.CLOSING;
     }
   }
 }
