8fea6221ecee8eb81cd300128bcf644610bde409
Kevin Kuryshev <kuryshevk@gmail.com>
2022-03-16 00:20:22 +0100

    Removed unnecessary import

--- satviz-sat/src/test/java/edu/kit/satviz/sat/ClauseTest.java
+++ satviz-sat/src/test/java/edu/kit/satviz/sat/ClauseTest.java
@@ -2,7 +2,6 @@ package edu.kit.satviz.sat;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
