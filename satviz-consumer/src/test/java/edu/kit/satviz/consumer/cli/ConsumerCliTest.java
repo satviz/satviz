b8285e501169b9fb9c4d5399a795f32cc257d15e
Kevin Kuryshev <kuryshevk@gmail.com>
2022-03-22 00:35:42 +0100

    Add two more tests for the CLI (one fails but necessary changes will be made in next commit)

--- satviz-consumer/src/test/java/edu/kit/satviz/consumer/cli/ConsumerCliTest.java
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/cli/ConsumerCliTest.java
@@ -1,8 +1,5 @@
 package edu.kit.satviz.consumer.cli;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import edu.kit.satviz.common.Constraint;
 import edu.kit.satviz.common.ConstraintValidationException;
 import edu.kit.satviz.consumer.config.ConsumerConfig;
@@ -10,26 +7,36 @@ import edu.kit.satviz.consumer.config.ExternalModeConfig;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class ConsumerCliTest {
 
   private ConsumerConfig validConfig1;
-  private String[] arguments1;
+  private String[] validArguments1;
+  private String[] fileArgument;
   private String[] invalidArguments1;
+  private String[] invalidArguments2;
 
   @BeforeEach
   void setUp() throws IOException {
     Path tempInstance = Files.createTempFile("instance",".cnf");
     Path tempVideoTemplate = Files.createTempFile("video", ".ogv");
-    arguments1 = new String[] {
+    Path configPath = Paths.get(
+        String.valueOf(ConsumerCliTest.class.getResource("/config2.json"))
+    );
+
+    validArguments1 = new String[] {
         "-i", tempInstance.toString(), "-o", tempVideoTemplate.toString(), "external", "-P", "1231"
     };
-    invalidArguments1 = new String[] {
-        "external", "-P", "1231"
-    };
+    fileArgument = new String[] {"file", configPath.toString()};
+    invalidArguments1 = new String[] {"external", "-P", "1231"};
+    invalidArguments2 = new String[] {"embedded"}; // should not throw NullPointerException
+
     validConfig1 = new ConsumerConfig();
     ExternalModeConfig externalModeConfig = new ExternalModeConfig();
     externalModeConfig.setPort(1231);
@@ -39,16 +46,28 @@ class ConsumerCliTest {
   }
 
   @Test
-  void test_parseArgs_validArgumentsWithValidation()
+  void test_parseArgs_valid_withValidation()
       throws ArgumentParserException, ConstraintValidationException {
-    ConsumerConfig config = ConsumerCli.parseArgs(arguments1);
+    ConsumerConfig config = ConsumerCli.parseArgs(validArguments1);
     (new ConsumerConstraint()).validate(config);
     assertEquals(validConfig1, config);
   }
 
   @Test
-  void test_parseArgs_withoutInstance() throws ArgumentParserException {
+  void test_parseArgs_valid_withFile() {
+    assertDoesNotThrow(() -> ConsumerCli.parseArgs(fileArgument));
+  }
+
+  @Test
+  void test_parseArgs_invalid_withoutInstance() throws ArgumentParserException {
     ConsumerConfig config = ConsumerCli.parseArgs(invalidArguments1);
+    Constraint<ConsumerConfig> inputConstraint = new ConsumerConstraint();
+    assertThrows(ConstraintValidationException.class, () -> inputConstraint.validate(config));
+  }
+
+  @Test
+  void test_parseArgs_invalid_tooFewArguments() throws ArgumentParserException {
+    ConsumerConfig config = ConsumerCli.parseArgs(invalidArguments2);
     Constraint<ConsumerConfig> inputConstraint = new ConsumerConstraint();
     assertThrows(ConstraintValidationException.class, () -> inputConstraint.validate(config));
   }
