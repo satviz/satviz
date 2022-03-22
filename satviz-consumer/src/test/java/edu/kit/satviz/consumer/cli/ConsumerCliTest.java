9d734ff1e3b029f4c9b5d530d6c49311d2c475bf
Kevin Kuryshev <kuryshevk@gmail.com>
2022-03-22 20:12:22 +0100

    Improve style within ConsumerCliTest class

--- satviz-consumer/src/test/java/edu/kit/satviz/consumer/cli/ConsumerCliTest.java
+++ satviz-consumer/src/test/java/edu/kit/satviz/consumer/cli/ConsumerCliTest.java
@@ -23,144 +23,132 @@ import org.junit.jupiter.api.Test;
 
 class ConsumerCliTest {
 
-  private ConsumerConfig validConfig1;
-  private ConsumerConfig validConfig2;
-  private ConsumerConfig validConfig3;
-  private ConsumerConfig validConfig4;
-  private String[] externalArguments;
-  private String[] embeddedSolverArguments;
-  private String[] embeddedProofArguments;
-  private String[] fileArgument;
-  private String[] colorArguments;
-  private String[] invalidArguments1;
-  private String[] invalidArguments2;
-  private String[] invalidArguments3;
+  private Path tempInstance;
+  private Path tempVideoTemplate;
 
   @BeforeEach
   void setUp() throws IOException {
-    Path tempInstance = Files.createTempFile("instance", ".cnf");
-    Path tempVideoTemplate = Files.createTempFile("video", ".ogv");
-    Path configPath = Files.createTempFile("config", ".json");
-    Files.copy(
-        Objects.requireNonNull(ConsumerCliTest.class.getResourceAsStream("/config2.json")),
-        configPath,
-        StandardCopyOption.REPLACE_EXISTING
-    );
-
-    externalArguments = new String[] {
-        "-i", tempInstance.toString(),
-        "-o", tempVideoTemplate.toString(),
-        "external", "-P", "1231"
-    };
-    Path tempSolver = Files.createTempFile("solver", ".so");
-    embeddedSolverArguments = new String[] {
-        "-i", tempInstance.toString(),
-        "embedded", "-s", tempSolver.toString()
-    };
-    Path tempProof = Files.createTempFile("proof", ".drat");
-    embeddedProofArguments = new String[] {
-        "-i", tempInstance.toString(),
-        "embedded", "-p", tempProof.toString()
-    };
-    fileArgument = new String[] {"config", configPath.toString()};
-    colorArguments = new String[] {
-        "-i", tempInstance.toString(),
-        "-c", "#000000:#FFFFFF",
-        "-o", tempVideoTemplate.toString(),
-        "external", "-P", "1231"
-    };
-    invalidArguments1 = new String[] {"external", "-P", "1231"};
-    invalidArguments2 = new String[] {"embedded"};
-    invalidArguments3 = new String[] {
-        "-i", tempInstance.toString(), "-c", "000000:#FFFFFF", "external", "-P", "1231"
-    };
-
-    validConfig1 = getExternalConfig(tempInstance, tempVideoTemplate);
-    validConfig2 = getEmbeddedConfig(tempInstance, EmbeddedModeSource.SOLVER, tempSolver);
-    validConfig3 = getEmbeddedConfig(tempInstance, EmbeddedModeSource.PROOF, tempProof);
-    validConfig4 = getExternalConfig(tempInstance, tempVideoTemplate);
-    HeatmapColors heatmapColors = new HeatmapColors();
-    heatmapColors.setFromColor(0x000000);
-    heatmapColors.setToColor(0xffffff);
-    validConfig4.setHeatmapColors(heatmapColors);
+    tempInstance = Files.createTempFile("instance", ".cnf");
+    tempVideoTemplate = Files.createTempFile("video", ".ogv");
   }
 
-  private ConsumerConfig getExternalConfig(Path instance, Path tempVideoTemplate) {
+  private ConsumerConfig getExternalConfig() {
     var config = new ConsumerConfig();
     var externalModeConfig = new ExternalModeConfig();
     externalModeConfig.setPort(1231);
     config.setModeConfig(externalModeConfig);
-    config.setInstancePath(instance);
+    config.setInstancePath(tempInstance);
     config.setVideoTemplatePath(tempVideoTemplate.toString());
     return config;
   }
 
   private ConsumerConfig getEmbeddedConfig(
-      Path instance, EmbeddedModeSource sourceMode, Path source
+      EmbeddedModeSource sourceMode, Path source
   ) {
     var config = new ConsumerConfig();
     var embeddedModeConfig = new EmbeddedModeConfig();
     embeddedModeConfig.setSource(sourceMode);
     embeddedModeConfig.setSourcePath(source);
     config.setModeConfig(embeddedModeConfig);
-    config.setInstancePath(instance);
+    config.setInstancePath(tempInstance);
     return config;
   }
 
   @Test
   void test_parseArgs_valid_external()
       throws ArgumentParserException, ConstraintValidationException {
-    ConsumerConfig config = ConsumerCli.parseArgs(externalArguments);
-    (new ConsumerConstraint()).validate(config);
-    assertEquals(validConfig1, config);
+    var arguments = new String[] {
+        "-i", tempInstance.toString(),
+        "-o", tempVideoTemplate.toString(),
+        "external", "-P", "1231"
+    };
+    var config = getExternalConfig();
+    var result = ConsumerCli.parseArgs(arguments);
+    (new ConsumerConstraint()).validate(result);
+    assertEquals(config, result);
   }
 
   @Test
   void test_parseArgs_valid_embeddedSolver()
-      throws ArgumentParserException, ConstraintValidationException {
-    ConsumerConfig config = ConsumerCli.parseArgs(embeddedSolverArguments);
-    (new ConsumerConstraint()).validate(config);
-    assertEquals(validConfig2, config);
+      throws ArgumentParserException, ConstraintValidationException, IOException {
+    Path tempSolver = Files.createTempFile("solver", ".so");
+    var arguments = new String[] {
+        "-i", tempInstance.toString(),
+        "embedded", "-s", tempSolver.toString()
+    };
+    var config = getEmbeddedConfig(EmbeddedModeSource.SOLVER, tempSolver);
+    var result = ConsumerCli.parseArgs(arguments);
+    (new ConsumerConstraint()).validate(result);
+    assertEquals(config, result);
   }
 
   @Test
   void test_parseArgs_valid_embeddedProof()
-      throws ArgumentParserException, ConstraintValidationException {
-    ConsumerConfig config = ConsumerCli.parseArgs(embeddedProofArguments);
-    (new ConsumerConstraint()).validate(config);
-    assertEquals(validConfig3, config);
+      throws ArgumentParserException, ConstraintValidationException, IOException {
+    Path tempProof = Files.createTempFile("proof", ".drat");
+    var arguments = new String[] {
+        "-i", tempInstance.toString(),
+        "embedded", "-p", tempProof.toString()
+    };
+    var config = getEmbeddedConfig(EmbeddedModeSource.PROOF, tempProof);
+    var result = ConsumerCli.parseArgs(arguments);
+    (new ConsumerConstraint()).validate(result);
+    assertEquals(config, result);
   }
 
   @Test
-  void test_parseArgs_valid_withFile() {
-    assertDoesNotThrow(() -> ConsumerCli.parseArgs(fileArgument));
+  void test_parseArgs_valid_withFile() throws IOException {
+    Path configPath = Files.createTempFile("config", ".json");
+    Files.copy(
+        Objects.requireNonNull(ConsumerCliTest.class.getResourceAsStream("/config2.json")),
+        configPath,
+        StandardCopyOption.REPLACE_EXISTING
+    );
+    var arguments = new String[] {"config", configPath.toString()};
+    assertDoesNotThrow(() -> ConsumerCli.parseArgs(arguments));
   }
 
   @Test
   void test_parseArgs_valid_colors()
       throws ArgumentParserException, ConstraintValidationException {
-    ConsumerConfig config = ConsumerCli.parseArgs(colorArguments);
-    (new ConsumerConstraint()).validate(config);
-    assertEquals(validConfig4, config);
+    var arguments = new String[] {
+        "-i", tempInstance.toString(),
+        "-c", "#000000:#FFFFFF",
+        "-o", tempVideoTemplate.toString(),
+        "external", "-P", "1231"
+    };
+    var config = getExternalConfig();
+    HeatmapColors heatmapColors = new HeatmapColors();
+    heatmapColors.setFromColor(0x000000);
+    heatmapColors.setToColor(0xffffff);
+    config.setHeatmapColors(heatmapColors);
+    var result = ConsumerCli.parseArgs(arguments);
+    (new ConsumerConstraint()).validate(result);
+    assertEquals(config, result);
   }
 
   @Test
   void test_parseArgs_invalid_withoutInstance() throws ArgumentParserException {
-    ConsumerConfig config = ConsumerCli.parseArgs(invalidArguments1);
+    var arguments = new String[] {"external", "-P", "1231"};
+    var config = ConsumerCli.parseArgs(arguments);
     Constraint<ConsumerConfig> inputConstraint = new ConsumerConstraint();
     assertThrows(ConstraintValidationException.class, () -> inputConstraint.validate(config));
   }
 
   @Test
   void test_parseArgs_invalid_tooFewArguments() throws ArgumentParserException {
-    ConsumerConfig config = ConsumerCli.parseArgs(invalidArguments2);
+    var arguments = new String[] {"embedded"};
+    var config = ConsumerCli.parseArgs(arguments);
     Constraint<ConsumerConfig> inputConstraint = new ConsumerConstraint();
     assertThrows(ConstraintValidationException.class, () -> inputConstraint.validate(config));
   }
 
   @Test
   void test_parseArgs_invalidColors() {
-    assertThrows(ArgumentParserException.class, () -> ConsumerCli.parseArgs(invalidArguments3));
+    var arguments = new String[] {
+        "-i", tempInstance.toString(), "-c", "000000:#FFFFFF", "external", "-P", "1231"
+    };
+    assertThrows(ArgumentParserException.class, () -> ConsumerCli.parseArgs(arguments));
   }
 
 }
